a62e46eb80dc5875d30681d70f34b956d917818f
Shawn Xu <xu107288696@gmail.com>
2024-02-11 13:50:34 -0800

    Reduce depth even more for nodes with very high expected reductions

--- src/search.cpp
+++ src/search.cpp
@@ -1184,7 +1184,7 @@ moves_loop:  // When in check, search starts here
                 r += 2;
 
             // Note that if expected reduction is high, we reduce search depth by 1 here (~9 Elo)
-            value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth - (r > 3), !cutNode);
+            value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth - (r > 3) - (r > 7), !cutNode);
         }
 
         // For PV nodes only, do a full PV search on the first move or after a fail high,
