7d030ccbad4286c96e3837da6a63ac6d9b69d50c
Shawn Xu <xu107288696@gmail.com>
2024-02-11 17:49:12 -0800

    bench 1027182

--- src/search.cpp
+++ src/search.cpp
@@ -1169,6 +1169,10 @@ moves_loop:  // When in check, search starts here
             if (!ttMove)
                 r += 2;
 
+            // Increase reduction if move comes second in order and skipped LMR (~? Elo)
+            else if (rootNode && moveCount == 2)
+                r ++;
+
             // Note that if expected reduction is high, we reduce search depth by 1 here (~9 Elo)
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth - (r > 3), !cutNode);
         }
