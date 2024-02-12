a57ebb7cb932bb1585d399be1e05d67ea064c3d6
Shawn Xu <xu107288696@gmail.com>
2024-02-11 19:37:58 -0800

    bench 1040006

--- src/search.cpp
+++ src/search.cpp
@@ -1144,11 +1144,10 @@ moves_loop:  // When in check, search starts here
             if (value > alpha && d < newDepth)
             {
                 // Adjust full-depth search based on LMR results - if the result
-                // was good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch    = value > (bestValue + 49 + 2 * newDepth);  // (~1 Elo)
+                // was bad enough search shallower.
                 const bool doShallowerSearch = value < bestValue + newDepth;             // (~2 Elo)
 
-                newDepth += doDeeperSearch - doShallowerSearch;
+                newDepth -= doShallowerSearch;
 
                 if (newDepth > d)
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
