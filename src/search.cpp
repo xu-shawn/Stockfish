0a7640ef725c5272929fb11b2a2f19921c9555ef
Shawn Xu <xu107288696@gmail.com>
2024-02-18 22:02:13 -0800

    do not nullify some reduction adjustments for ttMoves

--- src/search.cpp
+++ src/search.cpp
@@ -1110,6 +1110,11 @@ moves_loop:  // When in check, search starts here
         if (ttCapture)
             r++;
 
+        // Set reduction to 0 for first picked move (ttMove) (~2 Elo)
+        // Nullifies all previous reduction adjustments to ttMove and leaves only history to do them
+        if (move == ttMove)
+            r = 0;
+
         // Decrease reduction for PvNodes (~3 Elo)
         if (PvNode)
             r--;
@@ -1121,11 +1126,6 @@ moves_loop:  // When in check, search starts here
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
             r++;
-
-        // Set reduction to 0 for first picked move (ttMove) (~2 Elo)
-        // Nullifies all previous reduction adjustments to ttMove and leaves only history to do them
-        else if (move == ttMove)
-            r = 0;
 
         ss->statScore = 2 * thisThread->mainHistory[us][move.from_to()]
                       + (*contHist[0])[movedPiece][move.to_sq()]
