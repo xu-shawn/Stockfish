21b2c45b2ac3e32244a181a8ef7f7df78fc17711
Shawn Xu <xu107288696@gmail.com>
2024-02-20 13:37:25 -0800

    simplify doShallowerSearch

--- src/search.cpp
+++ src/search.cpp
@@ -1153,9 +1153,8 @@ moves_loop:  // When in check, search starts here
                 // Adjust full-depth search based on LMR results - if the result
                 // was good enough search deeper, if it was bad enough search shallower.
                 const bool doDeeperSearch    = value > (bestValue + 49 + 2 * newDepth);  // (~1 Elo)
-                const bool doShallowerSearch = value < bestValue;             // (~2 Elo)
 
-                newDepth += doDeeperSearch - doShallowerSearch;
+                newDepth += doDeeperSearch;
 
                 if (newDepth > d)
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
