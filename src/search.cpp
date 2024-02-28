bbabda4726b848d58b5c572b8d0ca71593595c2f
Shawn Xu <xu107288696@gmail.com>
2024-02-27 17:14:17 -0800

    try something

--- src/search.cpp
+++ src/search.cpp
@@ -964,7 +964,7 @@ moves_loop:  // When in check, search starts here
                 {
                     Piece capturedPiece = pos.piece_on(move.to_sq());
                     int   futilityEval =
-                      ss->staticEval + 277 + 292 * lmrDepth + PieceValue[capturedPiece]
+                      ss->staticEval + 277 + 292 * lmrDepth + PieceValue[capturedPiece] * 2
                       + thisThread->captureHistory[movedPiece][move.to_sq()][type_of(capturedPiece)]
                           / 7;
                     if (futilityEval < alpha)
