bdde89d6c393202dae2ea4db94d079b7c21101fb
Shawn Xu <xu107288696@gmail.com>
2024-03-05 10:22:15 -0800

    increase reduction on repetition when ttValue > 0

--- src/search.cpp
+++ src/search.cpp
@@ -1111,7 +1111,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction on repetition (~1 Elo)
         if (move == (ss - 4)->currentMove && pos.has_repeated())
-            r += 2;
+            r += 2 + (ttValue > 0) * 2;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
