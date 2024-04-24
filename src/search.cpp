b0b1fc48ab1c078a36d32b90ba74021849b6b21a
Shawn Xu <xu107288696@gmail.com>
2024-04-24 12:10:55 -0700

    reduce qext margin
    bench 1396410

--- src/search.cpp
+++ src/search.cpp
@@ -1088,11 +1088,11 @@ moves_loop:  // When in check, search starts here
                     {
                         extension = 2 + (value < singularBeta - 11 && !ttCapture);
 
-                        if (value < singularBeta - 300 && !ttCapture && (ss + 1)->cutoffCnt > 3
+                        if (value < singularBeta - 250 && !ttCapture && (ss + 1)->cutoffCnt > 3
                             && tte->depth() >= depth - 2 && ttValue > alpha)
                         {
-                            singularBeta -= 300;
-                            singularDepth = newDepth;
+                            singularBeta -= 250;
+                            singularDepth = newDepth * 2 / 3;
 
                             ss->excludedMove  = move;
                             int moveCountPrev = ss->moveCount;
