4fdcf7ce53d88f2ffbebcb92dc85c2c927bd0a90
Shawn Xu <xu107288696@gmail.com>
2024-04-23 19:59:36 -0700

    add a condition from Viren's test
    bench 1405831

--- src/search.cpp
+++ src/search.cpp
@@ -1055,7 +1055,7 @@ moves_loop:  // When in check, search starts here
                         extension = 2 + (value < singularBeta - 11 && !ttCapture);
 
                         if (value < singularBeta - 300 && !ttCapture && (ss + 1)->cutoffCnt > 3
-                            && tte->depth() >= depth - 2)
+                            && tte->depth() >= depth - 2 && ttValue > alpha)
                         {
                             singularBeta -= 300;
                             singularDepth = newDepth * 2 / 3;
@@ -1065,7 +1065,7 @@ moves_loop:  // When in check, search starts here
                             value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta,
                                                   singularDepth, cutNode);
                             ss->excludedMove = Move::none();
-                            ss->moveCount += moveCountPrev;
+                            ss->moveCount    = moveCountPrev;
 
                             extension += value < singularBeta;
                         }
