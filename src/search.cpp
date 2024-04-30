6f9d9cb6e34ee1c602aa43cb095affb29f31fa14
Shawn Xu <xu107288696@gmail.com>
2024-04-29 17:34:43 -0700

    quad extend only when move gives check
    bench 1558948

--- src/search.cpp
+++ src/search.cpp
@@ -1055,8 +1055,8 @@ moves_loop:  // When in check, search starts here
                     {
                         extension = 2 + (value < singularBeta - 11 && !ttCapture);
 
-                        if (value < singularBeta - 300 && !ttCapture && (ss + 1)->cutoffCnt > 3
-                            && tte->depth() >= depth - 2)
+                        if (value < singularBeta - 300 && !ttCapture && givesCheck
+                            && (ss + 1)->cutoffCnt > 3 && tte->depth() >= depth - 2)
                         {
                             singularBeta -= 300;
                             singularDepth = newDepth * 2 / 3;
