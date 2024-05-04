e109d36afb129bf7cc41db27761329308e7a8c55
Shawn Xu <xu107288696@gmail.com>
2024-05-04 01:05:49 -0700

    fix again
    bench 1416158

--- src/search.cpp
+++ src/search.cpp
@@ -237,7 +237,7 @@ void Search::Worker::iterative_deepening() {
     // Allocate stack with extra size to allow access from (ss - 7) to (ss + 2):
     // (ss - 7) is needed for update_continuation_histories(ss - 1) which accesses (ss - 6),
     // (ss + 2) is needed for initialization of cutOffCnt and killers.
-    Stack  stack[MAX_PLY + 10] = {};
+    Stack  stack[MAX_PLY + 11] = {};
     Stack* ss                  = stack + 8;
 
     for (int i = 8; i > 0; --i)
