da425827271ba650b9352906262a6f209193886d
Shawn Xu <xu107288696@gmail.com>
2024-05-04 21:10:20 -0700

    fixed mistake
    bench 1564781

--- src/search.cpp
+++ src/search.cpp
@@ -1083,7 +1083,7 @@ moves_loop:  // When in check, search starts here
 
                 // If the ttMove is assumed to fail high over current beta (~7 Elo)
                 else if (ttValue >= beta)
-                    extension = -3 - ttCapture && !PvNode;
+                    extension = -3 - (ttCapture && !PvNode);
 
                 // If we are on a cutNode but the ttMove is not assumed to fail high over current beta (~1 Elo)
                 else if (cutNode)
