7d04126ecb6462274c96f21d773a4dd1e139da49
Shawn Xu <xu107288696@gmail.com>
2024-05-04 19:02:44 -0700

    attempt 3
    bench 1511880

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -180,11 +180,11 @@ void MovePicker::score() {
             // histories
             m.value = 2 * (*mainHistory)[pos.side_to_move()][m.from_to()];
             m.value += 2 * (*pawnHistory)[pawn_structure_index(pos)][pc][to];
-            m.value += 2 * (*continuationHistory[0])[pc][to];
+            m.value += 5 * (*continuationHistory[0])[pc][to] / 2;
             m.value += 3 * (*continuationHistory[1])[pc][to] / 2;
             m.value += (*continuationHistory[2])[pc][to] / 4;
-            m.value += (*continuationHistory[3])[pc][to];
-            m.value += (*continuationHistory[5])[pc][to];
+            m.value += (*continuationHistory[3])[pc][to] / 2;
+            m.value += (*continuationHistory[5])[pc][to] / 2;
 
             // bonus for checks
             m.value += bool(pos.check_squares(pt) & to) * 16384;
