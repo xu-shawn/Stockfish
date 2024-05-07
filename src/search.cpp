8167f6091e7120e68c5abe167dca890b0f4733d9
Shawn Xu <xu107288696@gmail.com>
2024-05-07 08:01:38 -0700

    ltc tune 2 largest values
    bench 1949667

--- src/search.cpp
+++ src/search.cpp
@@ -1084,7 +1084,7 @@ moves_loop:  // When in check, search starts here
 
                 // If the ttMove is assumed to fail high over current beta (~7 Elo)
                 else if (ttValue >= beta)
-                    extension = -3;
+                    extension = -3 - (!PvNode && (ss + 1)->cutoffCnt <= 3);
 
                 // If we are on a cutNode but the ttMove is not assumed to fail high over current beta (~1 Elo)
                 else if (cutNode)
