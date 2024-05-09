64c736af98fc6b5716a3fb174ac24c8788837a57
Shawn Xu <xu107288696@gmail.com>
2024-05-09 11:12:12 -0700

    1/8 8-conthist
    bench 2357002

--- src/search.cpp
+++ src/search.cpp
@@ -1787,7 +1787,7 @@ void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
         if (ss->inCheck && i > 2)
             break;
         if (((ss - i)->currentMove).is_ok())
-            (*(ss - i)->continuationHistory)[pc][to] << bonus / (1 + 3 * (i == 3) + 15 * (i == 8));
+            (*(ss - i)->continuationHistory)[pc][to] << bonus / (1 + 3 * (i == 3) + 7 * (i == 8));
     }
 }
 
