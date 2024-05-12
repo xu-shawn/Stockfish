642ae5249c63dafd8a58df4a0d2add257e049df8
Shawn Xu <xu107288696@gmail.com>
2024-05-12 16:10:51 -0700

    reverse effect
    bench 2295796

--- src/search.cpp
+++ src/search.cpp
@@ -779,7 +779,7 @@ Value Search::Worker::search(
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (!PvNode && (ss - 1)->currentMove != Move::null() && (ss - 1)->statScore < 16993
-        && eval >= beta && ss->staticEval >= beta - (19 - cutNode) * depth + 326 && !excludedMove
+        && eval >= beta && ss->staticEval >= beta - (19 + cutNode) * depth + 326 && !excludedMove
         && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
         && beta > VALUE_TB_LOSS_IN_MAX_PLY)
     {
