4c402267be3b49bd77247dcb5087e00c42371cc1
Shawn Xu <xu107288696@gmail.com>
2024-05-14 11:27:11 -0700

    dbg_mean_of attempt 2
    bench 1454344

--- src/search.cpp
+++ src/search.cpp
@@ -779,7 +779,7 @@ Value Search::Worker::search(
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (!PvNode && (ss - 1)->currentMove != Move::null() && (ss - 1)->statScore < 16079
-        && eval >= beta && ss->staticEval >= beta - 21 * depth + 324 + ss->statScore / 1000
+        && eval >= beta && ss->staticEval >= beta - 21 * depth + 324 + (ss->statScore + 6000) / 1000
         && !excludedMove && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
         && beta > VALUE_TB_LOSS_IN_MAX_PLY)
     {
