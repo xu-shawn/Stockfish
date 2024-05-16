ccbce70b206994861991fb7a3e5f0a87a222b962
Shawn Xu <xu107288696@gmail.com>
2024-05-15 17:20:02 -0700

    less reductions after depth 18
    bench 1713639

--- src/search.cpp
+++ src/search.cpp
@@ -787,7 +787,7 @@ Value Search::Worker::search(
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 144, 6) + depth / 4 + int(std::log(depth)) + 4;
+        Depth R = std::min(int(eval - beta) / 144, 6) + depth / 5 + int(std::log(depth + 1)) + 4;
 
         ss->currentMove         = Move::null();
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
