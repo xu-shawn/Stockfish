3e5e233a90107b1438f3dcb8186bc1fd0ca1a4cc
Shawn Xu <xu107288696@gmail.com>
2024-05-15 23:24:00 -0700

    reverse idea
    bench 1831813

--- src/search.cpp
+++ src/search.cpp
@@ -780,8 +780,8 @@ Value Search::Worker::search(
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (!PvNode && (ss - 1)->currentMove != Move::null() && (ss - 1)->statScore < 16079
-        && eval >= beta && ss->staticEval >= beta - 21 * depth + 324 && !excludedMove
-        && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
+        && eval >= beta && ss->staticEval >= beta - 21 * depth + 324 + (!cutNode) * 20
+        && !excludedMove && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
         && beta > VALUE_TB_LOSS_IN_MAX_PLY)
     {
         assert(eval - beta >= 0);
