8faca04d5e86a673156a8c6a3cd92b9677682316
Shawn Xu <xu107288696@gmail.com>
2024-05-19 18:12:15 -0700

    hope this scales
    bench 2288194

--- src/search.cpp
+++ src/search.cpp
@@ -1141,7 +1141,7 @@ moves_loop:  // When in check, search starts here
         // Set reduction to 0 for first picked move (ttMove) (~2 Elo)
         // Nullifies all previous reduction adjustments to ttMove and leaves only history to do them
         else if (move == ttMove)
-            r = 0;
+            r = -(!ss->ttPv);
 
         ss->statScore = 2 * thisThread->mainHistory[us][move.from_to()]
                       + (*contHist[0])[movedPiece][move.to_sq()]
