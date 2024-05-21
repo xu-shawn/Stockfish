322f36890fb8da372d27cbec01b3229ce9c33a87
Shawn Xu <xu107288696@gmail.com>
2024-05-20 20:39:21 -0700

    simplify away cutoffcnt reduction
    bench 2234846

--- src/search.cpp
+++ src/search.cpp
@@ -1133,10 +1133,6 @@ moves_loop:  // When in check, search starts here
         if (PvNode)
             r--;
 
-        // Increase reduction if next ply has a lot of fail high (~5 Elo)
-        if ((ss + 1)->cutoffCnt > 3)
-            r++;
-
         // Set reduction to 0 for first picked move (ttMove) (~2 Elo)
         // Nullifies all previous reduction adjustments to ttMove and leaves only history to do them
         else if (move == ttMove)
