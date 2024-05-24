a301615493320486f0a01875aeb41b2ea0090187
Shawn Xu <xu107288696@gmail.com>
2024-05-23 21:35:51 -0700

    wrong sign
    bench 1735978

--- src/search.cpp
+++ src/search.cpp
@@ -1002,7 +1002,7 @@ moves_loop:  // When in check, search starts here
                   (*contHist[0])[movedPiece][move.to_sq()]
                   + (*contHist[1])[movedPiece][move.to_sq()]
                   + 2 * thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()]
-                  - 1100;
+                  + 1100;
 
                 // Continuation history based pruning (~2 Elo)
                 if (lmrDepth < 6 && history < -4427 * depth)
