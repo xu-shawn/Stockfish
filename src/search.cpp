237608ce4278646f79d90b9e34726d33fdc2266b
Shawn Xu <xu107288696@gmail.com>
2024-05-23 23:31:38 -0700

    revert unrelated changes [pawn history in statscore]
    bench 1370734

--- src/search.cpp
+++ src/search.cpp
@@ -1133,7 +1133,7 @@ moves_loop:  // When in check, search starts here
 
         // Decrease reduction for PvNodes (~0 Elo on STC, ~2 Elo on LTC)
         if (PvNode)
-            r -= 1 + (move == ttMove);
+            r--;
 
         // These reduction adjustments have no proven non-linear scaling.
 
