6167533fc65c8e7a4d3438f511e28e5126308a6a
Shawn Xu <xu107288696@gmail.com>
2024-05-29 21:20:15 -0700

    tuned 180k games
    bench 1515433

--- src/search.cpp
+++ src/search.cpp
@@ -578,9 +578,10 @@ Value Search::Worker::search(
         // Step 2. Check for aborted search and immediate draw
         if (threads.stop.load(std::memory_order_relaxed) || pos.is_draw(ss->ply)
             || ss->ply >= MAX_PLY)
-            return (ss->ply >= MAX_PLY && !ss->inCheck) ? evaluate(
-                     networks[numaAccessToken], pos, refreshTable, thisThread->optimism[us])
-                                                        : value_draw(thisThread->nodes);
+            return (ss->ply >= MAX_PLY && !ss->inCheck)
+                   ? evaluate(networks[numaAccessToken], pos, refreshTable,
+                              thisThread->optimism[us])
+                   : value_draw(thisThread->nodes);
 
         // Step 3. Mate distance pruning. Even if we mate at the next move our score
         // would be at best mate_in(ss->ply + 1), but if alpha is already bigger because
@@ -1341,10 +1342,12 @@ moves_loop:  // When in check, search starts here
     // Bonus for prior countermove that caused the fail low
     else if (!priorCapture && prevSq != SQ_NONE)
     {
-        int bonus = (116 * (depth > 5) + 115 * (PvNode || cutNode)
-                     + 186 * ((ss - 1)->statScore < -14144) + 121 * ((ss - 1)->moveCount > 9)
-                     + 64 * (!ss->inCheck && bestValue <= ss->staticEval - 115)
-                     + 137 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 81));
+        int bonus = (-21 + 183 * (depth > 5) + 95 * (PvNode || cutNode)
+                     + 264 * ((ss - 1)->statScore < -14144) + 164 * ((ss - 1)->moveCount > 9)
+                     + 68 * (!ss->inCheck && bestValue <= ss->staticEval - 115)
+                     + 129 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 81)
+                     + 57 * improving + 32 * opponentWorsening - 13 * (depth > 10)
+                     - 33 * (alpha - bestValue < 508) + 22 * ss->ttPv + 23 * (extension >= 2));
         update_continuation_histories(ss - 1, pos.piece_on(prevSq), prevSq,
                                       stat_bonus(depth) * bonus / 100);
         thisThread->mainHistory[~us][((ss - 1)->currentMove).from_to()]
