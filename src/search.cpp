77ea463c26caa32825ba5dc601a51e1e588e9f43
Shawn Xu <xu107288696@gmail.com>
2024-06-01 04:23:23 -0700

    tuned 177k games
    bench 1397026

--- src/search.cpp
+++ src/search.cpp
@@ -578,9 +578,10 @@ Value Search::Worker::search(
         // Step 2. Check for aborted search and immediate draw
         if (threads.stop.load(std::memory_order_relaxed) || pos.is_draw(ss->ply)
             || ss->ply >= MAX_PLY)
-            return (ss->ply >= MAX_PLY && !ss->inCheck) ? evaluate(
-                     networks[numaAccessToken], pos, refreshTable, thisThread->optimism[us])
-                                                        : value_draw(thisThread->nodes);
+            return (ss->ply >= MAX_PLY && !ss->inCheck)
+                   ? evaluate(networks[numaAccessToken], pos, refreshTable,
+                              thisThread->optimism[us])
+                   : value_draw(thisThread->nodes);
 
         // Step 3. Mate distance pruning. Even if we mate at the next move our score
         // would be at best mate_in(ss->ply + 1), but if alpha is already bigger because
@@ -1069,6 +1070,23 @@ moves_loop:  // When in check, search starts here
 
                     extension = 1 + (value < singularBeta - doubleMargin)
                               + (value < singularBeta - tripleMargin);
+
+                    if (value < singularBeta - 245 && !ttCapture && (ss + 1)->cutoffCnt > 3
+                        && tte->depth() >= depth - 2)
+                    {
+                        Value quadSingularBeta =
+                          singularBeta - 232 + (11 + 22 * (ss->ttPv && !PvNode)) * depth / 64;
+                        Depth quadSingularDepth = newDepth * 17 / 24;
+
+                        ss->excludedMove  = move;
+                        int moveCountPrev = ss->moveCount;
+                        value = search<NonPV>(pos, ss, quadSingularBeta - 1, quadSingularBeta,
+                                              quadSingularDepth, cutNode);
+                        ss->excludedMove = Move::none();
+                        ss->moveCount    = moveCountPrev;
+
+                        extension += value < singularBeta - 1;
+                    }
 
                     depth += ((!PvNode) && (depth < 16));
                 }
