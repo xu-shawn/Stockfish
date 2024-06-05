949871f50bb3444919aae65b250b5648f60b6a63
Shawn Xu <xu107288696@gmail.com>
2024-06-04 19:53:59 -0700

    higher slope
    bench 1144950

--- src/search.cpp
+++ src/search.cpp
@@ -1073,7 +1073,7 @@ moves_loop:  // When in check, search starts here
                 {
                     int doubleMargin = 304 * PvNode - 203 * !ttCapture;
                     int tripleMargin =
-                      105 + 259 * PvNode - 296 * !ttCapture + 97 * ss->ttPv + depth;
+                      95 + 259 * PvNode - 296 * !ttCapture + 97 * ss->ttPv + depth * 2;
 
                     extension = 1 + (value < singularBeta - doubleMargin)
                               + (value < singularBeta - tripleMargin);
