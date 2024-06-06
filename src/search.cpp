2c26ac087ccdbbcc691b933b031d6f4c4ff2e7ea
Shawn Xu <xu107288696@gmail.com>
2024-06-06 10:12:34 -0700

    history fill
    bench 1208586

--- src/search.cpp
+++ src/search.cpp
@@ -498,7 +498,7 @@ void Search::Worker::iterative_deepening() {
 
 void Search::Worker::clear() {
     counterMoves.fill(Move::none());
-    mainHistory.fill(0);
+    mainHistory.fill(-1000);
     captureHistory.fill(0);
     pawnHistory.fill(-1193);
     correctionHistory.fill(0);
