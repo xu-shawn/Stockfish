4acacb521682d5317f104972e988e988505a02b3
Shawn Xu <xu107288696@gmail.com>
2024-06-07 21:12:37 -0700

    movecount
    bench 1121987

--- src/search.cpp
+++ src/search.cpp
@@ -1303,7 +1303,11 @@ moves_loop:  // When in check, search starts here
                 {
                     // Reduce other moves if we have found at least one score improvement (~2 Elo)
                     if (depth > 2 && depth < 13 && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
+                    {
                         depth -= 2;
+                        if (move == ttMove)
+                            ss->moveCount = moveCount = moveCount + 2;
+                    }
 
                     assert(depth > 0);
                     alpha = value;  // Update alpha! Always alpha < beta
