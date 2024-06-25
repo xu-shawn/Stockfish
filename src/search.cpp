d294283b998a11e47fc672e085b6abba89f8cc27
Shawn Xu <xu107288696@gmail.com>
2024-06-25 11:20:11 -0700

    see what this does
    bench 1488020

--- src/search.cpp
+++ src/search.cpp
@@ -785,7 +785,8 @@ Value Search::Worker::search(
                - (ss - 1)->statScore / 263
              >= beta
         && eval >= beta && eval < VALUE_TB_WIN_IN_MAX_PLY && (!ttData.move || ttCapture))
-        return beta > VALUE_TB_LOSS_IN_MAX_PLY ? beta + (eval - beta) / 3 : eval;
+        return beta > VALUE_TB_LOSS_IN_MAX_PLY && (ss - 1)->excludedMove ? beta + (eval - beta) / 3
+                                                                         : eval;
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (!PvNode && (ss - 1)->currentMove != Move::null() && (ss - 1)->statScore < 14369
