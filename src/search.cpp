61bbfca10d03fbcf250ee00ae418615dd98b4b4d
Shawn Xu <xu107288696@gmail.com>
2024-06-26 09:09:57 -0700

    new ttCapture formula
    bench 1244843

--- src/search.cpp
+++ src/search.cpp
@@ -1152,7 +1152,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if ttMove is a capture (~3 Elo)
         if (ttCapture)
-            r++;
+            r += std::log(moveCount);
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
