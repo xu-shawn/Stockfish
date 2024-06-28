8f63367548be4d7781dcc65ac26f8bb16cad06e8
Shawn Xu <xu107288696@gmail.com>
2024-06-28 15:41:45 -0700

    nmp cutnode
    bench 1071793

--- src/search.cpp
+++ src/search.cpp
@@ -803,7 +803,7 @@ Value Search::Worker::search(
 
         pos.do_null_move(st, tt);
 
-        Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, depth - R, !cutNode);
+        Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, depth - R, true);
 
         pos.undo_null_move();
 
