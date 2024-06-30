ab911bd0a22c68df89ad6bcb4920894334295894
Shawn Xu <xu107288696@gmail.com>
2024-06-30 16:10:41 -0700

    no ttpv
    bench 1172551

--- src/search.cpp
+++ src/search.cpp
@@ -1061,7 +1061,7 @@ moves_loop:  // When in check, search starts here
                 && std::abs(ttData.value) < VALUE_TB_WIN_IN_MAX_PLY && (ttData.bound & BOUND_LOWER)
                 && ttData.depth >= depth - 3)
             {
-                Value singularBeta  = ttData.value - (52 + 80 * (ss->ttPv && !PvNode)) * depth / 64;
+                Value singularBeta  = ttData.value - depth;
                 Depth singularDepth = newDepth / 2;
 
                 ss->excludedMove = move;
