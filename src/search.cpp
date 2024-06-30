af7745bc3fce45c110377def1baa463108573c68
Shawn Xu <xu107288696@gmail.com>
2024-06-30 16:11:50 -0700

    combo patch
    bench 1278341

--- src/search.cpp
+++ src/search.cpp
@@ -796,7 +796,7 @@ Value Search::Worker::search(
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 197, 6) + depth / 3 + 5;
+        Depth R = std::min(int(eval - beta) / 197, 6) + depth / 3 + 4 + improving;
 
         ss->currentMove         = Move::null();
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
