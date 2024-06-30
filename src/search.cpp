902ce031e1720eeb4a05ac5d40b08e497d7c6c3b
Shawn Xu <xu107288696@gmail.com>
2024-06-30 14:27:10 -0700

    improving
    bench 1509768

--- src/search.cpp
+++ src/search.cpp
@@ -1154,6 +1154,9 @@ moves_loop:  // When in check, search starts here
         if (ttCapture)
             r++;
 
+        if (improving && !capture)
+            r--;
+
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
             r++;
