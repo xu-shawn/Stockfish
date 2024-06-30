b891ec5fe569289e0bb3532fa89139a7f5d47b70
Shawn Xu <xu107288696@gmail.com>
2024-06-30 15:20:28 -0700

    nmp improving
    bench 1005519

--- src/search.cpp
+++ src/search.cpp
@@ -789,8 +789,8 @@ Value Search::Worker::search(
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (!PvNode && (ss - 1)->currentMove != Move::null() && (ss - 1)->statScore < 14369
-        && eval >= beta && ss->staticEval >= beta - 21 * depth + 393 && !excludedMove
-        && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
+        && eval >= beta && ss->staticEval >= beta - 21 * depth + 393 - improving * 100
+        && !excludedMove && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
         && beta > VALUE_TB_LOSS_IN_MAX_PLY)
     {
         assert(eval - beta >= 0);
