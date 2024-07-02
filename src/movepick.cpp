4356581e77e9a8e8dbcaef225fdc88273140399b
Shawn Xu <xu107288696@gmail.com>
2024-07-02 01:05:03 -0700

    less weight to piecevalue in capture ordering
    bench 1184813

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -167,7 +167,7 @@ void MovePicker::score() {
     for (auto& m : *this)
         if constexpr (Type == CAPTURES)
             m.value =
-              7 * int(PieceValue[pos.piece_on(m.to_sq())])
+              5 * int(PieceValue[pos.piece_on(m.to_sq())])
               + (*captureHistory)[pos.moved_piece(m)][m.to_sq()][type_of(pos.piece_on(m.to_sq()))];
 
         else if constexpr (Type == QUIETS)
