fed2094539cde59376c8b8913211943beee75725
Shawn Xu <xu107288696@gmail.com>
2024-07-01 17:58:21 -0700

    obsidian razoring
    bench 1232575

--- src/search.cpp
+++ src/search.cpp
@@ -771,7 +771,7 @@ Value Search::Worker::search(
     // Step 7. Razoring (~1 Elo)
     // If eval is really low check with qsearch if it can exceed alpha, if it can't,
     // return a fail low.
-    if (eval < alpha - 512 - 293 * depth * depth)
+    if (eval < alpha - 512 - 293 * depth * depth && alpha < 2000)
     {
         value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
         if (value < alpha && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
