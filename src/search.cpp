0f329b308fca8da26b9d21d56f5205b894d50474
Shawn Xu <xu107288696@gmail.com>
2024-07-01 17:59:31 -0700

    potential scaler
    bench 1469847

--- src/search.cpp
+++ src/search.cpp
@@ -1312,7 +1312,7 @@ moves_loop:  // When in check, search starts here
                 {
                     // Reduce other moves if we have found at least one score improvement (~2 Elo)
                     if (depth > 2 && depth < 13 && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
-                        depth -= 2;
+                        depth -= 1;
 
                     assert(depth > 0);
                     alpha = value;  // Update alpha! Always alpha < beta
