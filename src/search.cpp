51ca97f55ed3095a0772d45810ee7eea84a63290
Shawn Xu <xu107288696@gmail.com>
2024-07-01 17:48:40 -0700

    more singext
    bench 1320365

--- src/search.cpp
+++ src/search.cpp
@@ -1059,7 +1059,7 @@ moves_loop:  // When in check, search starts here
             if (!rootNode && move == ttData.move && !excludedMove
                 && depth >= 4 - (thisThread->completedDepth > 35) + ss->ttPv
                 && std::abs(ttData.value) < VALUE_TB_WIN_IN_MAX_PLY && (ttData.bound & BOUND_LOWER)
-                && ttData.depth >= depth - 3)
+                && ttData.depth >= depth - 3 - 2 * (depth > 8))
             {
                 Value singularBeta  = ttData.value - (52 + 80 * (ss->ttPv && !PvNode)) * depth / 64;
                 Depth singularDepth = newDepth / 2;
