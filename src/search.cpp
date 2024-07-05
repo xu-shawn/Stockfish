ede1fb2f5b336a90d0feea0f2fb3d33d12e36679
Shawn Xu <xu107288696@gmail.com>
2024-07-04 23:39:10 -0700

    mcp simplification
    bench 1225999

--- src/search.cpp
+++ src/search.cpp
@@ -1009,9 +1009,7 @@ moves_loop:  // When in check, search starts here
         if (!rootNode && pos.non_pawn_material(us) && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
         {
             // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold (~8 Elo)
-            moveCountPruning =
-              moveCount >= futility_move_count(improving, depth)
-                             - (singularBound == BOUND_UPPER && singularValue < alpha - 50);
+            moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
             // Reduced depth of the next LMR search
             int lmrDepth = newDepth - r;
