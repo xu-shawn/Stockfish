2712e7dfc5745e9fc9d8bed5fdfcc495a8cff437
Shawn Xu <xu107288696@gmail.com>
2024-07-06 04:20:36 -0700

    quantum lmr
    bench 1259006

--- src/search.cpp
+++ src/search.cpp
@@ -1203,7 +1203,7 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore / 11125;
 
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
-        if (depth >= 2 && moveCount > 1 + rootNode)
+        if (depth >= 2 && moveCount > 1 + (rootNode && rootDepth & 1))
         {
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
