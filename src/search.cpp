bce1f3f1780b201530a79361f183ecb1e3a9b027
Shawn Xu <xu107288696@gmail.com>
2024-07-06 19:11:57 -0700

    simplify futility pruning condition
    bench 1080290

--- src/search.cpp
+++ src/search.cpp
@@ -788,8 +788,7 @@ Value Search::Worker::search(
         && eval - futility_margin(depth, cutNode && !ss->ttHit, improving, opponentWorsening)
                - (ss - 1)->statScore / 260
              >= beta
-        && eval >= beta && (!ttData.move || ttCapture) && beta > VALUE_TB_LOSS_IN_MAX_PLY
-        && eval < VALUE_TB_WIN_IN_MAX_PLY)
+        && eval >= beta && beta > VALUE_TB_LOSS_IN_MAX_PLY && eval < VALUE_TB_WIN_IN_MAX_PLY)
         return beta + (eval - beta) / 3;
 
     // Step 9. Null move search with verification search (~35 Elo)
