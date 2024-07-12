1d2ccd101ae290aa436f909c8774112f5ceecc38
Shawn Xu <xu107288696@gmail.com>
2024-07-11 23:51:00 -0700

    fix ub again
    bench 1238757

--- src/search.h
+++ src/search.h
@@ -65,7 +65,7 @@ struct Stack {
     int             ply;
     Move            currentMove;
     Move            excludedMove;
-    Move            killer;
+    Move            killer = Move::none();
     Value           staticEval;
     int             statScore;
     int             moveCount;
