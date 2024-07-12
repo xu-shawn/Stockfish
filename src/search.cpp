9d8f48a4114f4dabcb4f31f808ae783b9bb08519
Shawn Xu <xu107288696@gmail.com>
2024-07-11 17:51:48 -0700

    not ok
    bench 1301097

--- src/search.cpp
+++ src/search.cpp
@@ -1826,8 +1826,7 @@ void update_continuation_histories(Stack* ss, Piece pc, Square to, int bonus) {
         // Only update the first 2 continuation histories if we are in check
         if (ss->inCheck && i > 2)
             break;
-        if (((ss - i)->currentMove).is_ok())
-            (*(ss - i)->continuationHistory)[pc][to] << bonus / (1 + (i == 3));
+        (*(ss - i)->continuationHistory)[pc][to] << bonus / (1 + (i == 3));
     }
 }
 
