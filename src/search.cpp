eedd95b145bfaaafd0fbeaa5f01d8929a3d8d2cb
Shawn Xu <xu107288696@gmail.com>
2024-07-17 13:38:04 -0700

    simp doShallowerSearch
    bench 1460562

--- src/search.cpp
+++ src/search.cpp
@@ -1198,13 +1198,10 @@ moves_loop:  // When in check, search starts here
             {
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch    = value > (bestValue + 35 + 2 * newDepth);  // (~1 Elo)
-                const bool doShallowerSearch = value < bestValue + newDepth;             // (~2 Elo)
+                if (value > bestValue + 35 + 2 * newDepth)  // (~1 Elo)
+                    newDepth++;
 
-                newDepth += doDeeperSearch - doShallowerSearch;
-
-                if (newDepth > d)
-                    value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
+                value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
 
                 // Post LMR continuation history updates (~1 Elo)
                 int bonus = value <= alpha ? -stat_malus(newDepth)
