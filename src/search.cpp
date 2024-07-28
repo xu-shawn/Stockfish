cf073a528e4a85c3b8873f2762568d81b6907fe1
Shawn Xu <xu107288696@gmail.com>
2024-07-28 14:21:17 -0700

    probcut no cutnode
    bench 1273746

--- src/search.cpp
+++ src/search.cpp
@@ -865,7 +865,7 @@ Value Search::Worker::search(
             // If the qsearch held, perform the regular search
             if (value >= probCutBeta)
                 value =
-                  -search<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1, depth - 4, !cutNode);
+                  -search<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1, depth - 4, false);
 
             pos.undo_move(move);
 
