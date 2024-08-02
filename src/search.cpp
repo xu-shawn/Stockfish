55d900c43067049104ba59d0671195be9b593ad6
Shawn Xu <xu107288696@gmail.com>
2024-08-02 10:54:08 -0700

    lmr double extension
    bench 1453798

--- src/search.cpp
+++ src/search.cpp
@@ -1157,7 +1157,8 @@ moves_loop:  // When in check, search starts here
             // beyond the first move depth.
             // To prevent problems when the max value is less than the min value,
             // std::clamp has been replaced by a more robust implementation.
-            Depth d = std::max(1, std::min(newDepth - r, newDepth + 1));
+            Depth d = std::max(
+              1, std::min(newDepth - r, newDepth + 1 + (extension < 2 && !PvNode && !ttCapture)));
 
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, d, true);
 
