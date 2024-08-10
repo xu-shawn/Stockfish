568dde2043acf8be86a7a83650ed903761c6ac49
Shawn Xu <xu107288696@gmail.com>
2024-08-09 22:23:54 -0700

    tweak futility
    bench 1310115

--- src/search.cpp
+++ src/search.cpp
@@ -760,7 +760,9 @@ Value Search::Worker::search(
     // Step 8. Futility pruning: child node (~40 Elo)
     // The depth condition is important for mate finding.
     if (!ss->ttPv && depth < 13
-        && eval - futility_margin(depth, cutNode && !ss->ttHit, improving, opponentWorsening)
+        && eval
+               - futility_margin(depth, cutNode && eval == ss->staticEval, improving,
+                                 opponentWorsening)
                - (ss - 1)->statScore / 260
              >= beta
         && eval >= beta && (!ttData.move || ttCapture) && beta > VALUE_TB_LOSS_IN_MAX_PLY
