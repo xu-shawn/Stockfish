4da4b7f8c445514343d6c73cb5552896a554ecb6
Shawn Xu <xu107288696@gmail.com>
2024-08-09 22:04:38 -0700

    no ttpv same depth cutoff
    bench 1137760

--- src/search.cpp
+++ src/search.cpp
@@ -608,7 +608,7 @@ Value Search::Worker::search(
     // to save indentation, we list the condition in all code between here and there.
 
     // At non-PV nodes we check for an early TT cutoff
-    if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta)
+    if (!PvNode && !excludedMove && ttData.depth > depth - (ttData.value <= beta && !ss->ttPv)
         && ttData.value != VALUE_NONE  // Can happen when !ttHit or when access race in probe()
         && (ttData.bound & (ttData.value >= beta ? BOUND_LOWER : BOUND_UPPER)))
     {
