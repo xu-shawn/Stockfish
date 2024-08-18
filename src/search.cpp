3b7b9c50f2aff3dad7c90ffcdd63aed25cabd8da
Shawn Xu <xu107288696@gmail.com>
2024-08-18 14:04:33 -0600

    tweak dds
    bench 1498780

--- src/search.cpp
+++ src/search.cpp
@@ -1172,8 +1172,9 @@ moves_loop:  // When in check, search starts here
             {
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch    = value > (bestValue + 35 + 2 * newDepth);  // (~1 Elo)
-                const bool doShallowerSearch = value < bestValue + newDepth;             // (~2 Elo)
+                const bool doDeeperSearch =
+                  value > (bestValue + 35 + 2 * newDepth) || !(PvNode || cutNode);  // (~1 Elo)
+                const bool doShallowerSearch = value < bestValue + newDepth;        // (~2 Elo)
 
                 newDepth += doDeeperSearch - doShallowerSearch;
 
