daf5afee504cfbeb7475626732aaf4d1c51ca2ad
Shawn Xu <xu107288696@gmail.com>
2024-08-18 23:21:08 -0600

    take 2
    bench 1425626

--- src/position.h
+++ src/position.h
@@ -290,7 +290,7 @@ inline Key Position::key() const { return adjust_key50<false>(st->key); }
 
 template<bool AfterMove>
 inline Key Position::adjust_key50(Key k) const {
-    return st->rule50 < 14 - AfterMove ? k : k ^ make_key((st->rule50 - (14 - AfterMove)) / 8);
+    return st->rule50 < 20 - AfterMove ? k : k ^ make_key((st->rule50 - (20 - AfterMove)) / 8);
 }
 
 inline Key Position::pawn_key() const { return st->pawnKey; }
