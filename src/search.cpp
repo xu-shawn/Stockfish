66a821447773734c32f4cfb1e349e0430dae7eb3
Shawn Xu <xu107288696@gmail.com>
2024-08-19 13:03:44 -0600

    extra bonus
    bench 1321828

--- src/search.cpp
+++ src/search.cpp
@@ -1758,6 +1758,9 @@ void update_all_stats(const Position&      pos,
 
     if (!pos.capture_stage(bestMove))
     {
+        auto&     mainHist   = workerThread.mainHistory[pos.side_to_move()][bestMove.from_to()];
+        const int extraBonus = std::clamp(-(mainHist + 3000) / 100, 0, 100);
+        mainHist << extraBonus;
         update_quiet_histories(pos, ss, workerThread, bestMove, quietMoveBonus);
 
         // Decrease stats for all non-best quiet moves
