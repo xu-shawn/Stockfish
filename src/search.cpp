bd0047631010e1deac8bb185b09fb2d50d64f6d2
Shawn Xu <xu107288696@gmail.com>
2024-08-23 16:05:27 -0700

    take 2
    bench 1371990

--- src/search.cpp
+++ src/search.cpp
@@ -781,7 +781,7 @@ Value Search::Worker::search(
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 202, 6) + depth / 3 + 5 + game_phase(pos) / 8;
+        Depth R = std::min(int(eval - beta) / 202, 6) + depth / 3 + 3 + game_phase(pos) / 8;
 
         ss->currentMove         = Move::null();
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
