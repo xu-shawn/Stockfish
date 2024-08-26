d57f2be5bae9bbb2cb19c266d158fe0b5c24b3d1
Shawn Xu <xu107288696@gmail.com>
2024-08-25 18:42:26 -0700

    take 2
    bench 1460966

--- src/search.cpp
+++ src/search.cpp
@@ -81,9 +81,10 @@ constexpr int futility_move_count(bool improving, Depth depth) {
 // Add correctionHistory value to raw staticEval and guarantee evaluation
 // does not hit the tablebase range.
 Value to_corrected_static_eval(Value v, const Worker& w, const Position& pos) {
-    auto pcv = w.pawnCorrectionHistory[pos.side_to_move()][pawn_structure_index<Correction>(pos)];
-    auto mcv = w.materialCorrectionHistory[pos.side_to_move()][material_index(pos)];
-    auto cv  = (pcv + mcv) / 2;
+    const auto pcv =
+      w.pawnCorrectionHistory[pos.side_to_move()][pawn_structure_index<Correction>(pos)];
+    const auto mcv = w.materialCorrectionHistory[pos.side_to_move()][material_index(pos)];
+    const auto cv  = (2 * pcv + mcv) / 3;
     v += 66 * cv / 512;
     return std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
 }
