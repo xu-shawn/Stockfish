e84bfa3a580785a516cff764258a99e16da80038
Shawn Xu <xu107288696@gmail.com>
2024-09-08 00:59:07 -0700

    revert damage
    bench 1139625

--- src/search.cpp
+++ src/search.cpp
@@ -568,10 +568,9 @@ Value Search::Worker::search(
         // Step 2. Check for aborted search and immediate draw
         if (threads.stop.load(std::memory_order_relaxed) || pos.is_draw(ss->ply)
             || ss->ply >= MAX_PLY)
-            return (ss->ply >= MAX_PLY && !ss->inCheck)
-                   ? evaluate(networks[numaAccessToken], pos, refreshTable,
-                              thisThread->optimism[us])
-                   : value_draw(thisThread->nodes);
+            return (ss->ply >= MAX_PLY && !ss->inCheck) ? evaluate(
+                     networks[numaAccessToken], pos, refreshTable, thisThread->optimism[us])
+                                                        : value_draw(thisThread->nodes);
 
         // Step 3. Mate distance pruning. Even if we mate at the next move our score
         // would be at best mate_in(ss->ply + 1), but if alpha is already bigger because
@@ -1179,7 +1178,12 @@ moves_loop:  // When in check, search starts here
                 newDepth += doDeeperSearch - doShallowerSearch;
 
                 if (newDepth > d)
+                {
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
+
+                    if (!capture && value <= alpha)
+                        thisThread->mainHistory[us][move.from_to()] << -stat_malus(depth);
+                }
 
                 // Post LMR continuation history updates (~1 Elo)
                 int bonus = value >= beta ? stat_bonus(newDepth) : -stat_malus(newDepth);
