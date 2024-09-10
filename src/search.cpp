350bf888b7b7f2780131d07c0f1e086bf6611ecd
Shawn Xu <xu107288696@gmail.com>
2024-09-10 15:26:32 -0700

    non pawn negext/multicut
    bench 1580039

--- src/search.cpp
+++ src/search.cpp
@@ -1069,30 +1069,33 @@ moves_loop:  // When in check, search starts here
                     depth += ((!PvNode) && (depth < 16));
                 }
 
-                // Multi-cut pruning
-                // Our ttMove is assumed to fail high based on the bound of the TT entry,
-                // and if after excluding the ttMove with a reduced search we fail high
-                // over the original beta, we assume this expected cut-node is not
-                // singular (multiple moves fail high), and we can prune the whole
-                // subtree by returning a softbound.
-                else if (value >= beta && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
-                    return value;
+                else if (pos.non_pawn_material(us))
+                {
+                    // Multi-cut pruning
+                    // Our ttMove is assumed to fail high based on the bound of the TT entry,
+                    // and if after excluding the ttMove with a reduced search we fail high
+                    // over the original beta, we assume this expected cut-node is not
+                    // singular (multiple moves fail high), and we can prune the whole
+                    // subtree by returning a softbound.
+                    if (value >= beta && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
+                        return value;
 
-                // Negative extensions
-                // If other moves failed high over (ttValue - margin) without the
-                // ttMove on a reduced search, but we cannot do multi-cut because
-                // (ttValue - margin) is lower than the original beta, we do not know
-                // if the ttMove is singular or can do a multi-cut, so we reduce the
-                // ttMove in favor of other moves based on some conditions:
+                    // Negative extensions
+                    // If other moves failed high over (ttValue - margin) without the
+                    // ttMove on a reduced search, but we cannot do multi-cut because
+                    // (ttValue - margin) is lower than the original beta, we do not know
+                    // if the ttMove is singular or can do a multi-cut, so we reduce the
+                    // ttMove in favor of other moves based on some conditions:
 
-                // If the ttMove is assumed to fail high over current beta (~7 Elo)
-                else if (ttData.value >= beta)
-                    extension = -3;
+                    // If the ttMove is assumed to fail high over current beta (~7 Elo)
+                    else if (ttData.value >= beta)
+                        extension = -3;
 
-                // If we are on a cutNode but the ttMove is not assumed to fail high
-                // over current beta (~1 Elo)
-                else if (cutNode)
-                    extension = -2;
+                    // If we are on a cutNode but the ttMove is not assumed to fail high
+                    // over current beta (~1 Elo)
+                    else if (cutNode)
+                        extension = -2;
+                }
             }
 
             // Extension for capturing the previous moved piece (~1 Elo at LTC)
