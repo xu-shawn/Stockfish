28c3b3888612625fb3ff4d73a0ba7882fe12cdce
Shawn Xu <xu107288696@gmail.com>
2024-09-09 21:14:29 -0700

    logic tweaks
    bench 1746036

--- src/search.cpp
+++ src/search.cpp
@@ -1171,7 +1171,7 @@ moves_loop:  // When in check, search starts here
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, d, true);
 
             // Do a full-depth search when reduced LMR search fails high
-            if (value > alpha && d < newDepth && (PvNode && beta - alpha != 1))
+            if (value > alpha && d < newDepth && (!PvNode || beta - alpha != 1))
             {
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
