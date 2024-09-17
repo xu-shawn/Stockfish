6632a4c1481ef186f3a7c6427d334a52a3094be8
Shawn Xu <xu107288696@gmail.com>
2024-09-17 14:38:28 -0700

    the real ergo simplification
    bench 1210636

--- src/search.cpp
+++ src/search.cpp
@@ -67,7 +67,7 @@ namespace {
 // Futility margin
 Value futility_margin(Depth d, bool noTtCutNode, bool improving) {
     Value futilityMult       = 118 - 33 * noTtCutNode;
-    Value improvingDeduction = improving * futilityMult * 13 / 6;
+    Value improvingDeduction = improving * futilityMult * 7 / 3;
 
     return futilityMult * d - improvingDeduction;
 }
@@ -778,13 +778,8 @@ Value Search::Worker::search(
     // Step 8. Futility pruning: child node (~40 Elo)
     // The depth condition is important for mate finding.
     if (!ss->ttPv && depth < 13
-<<<<<<< HEAD
-        && eval - futility_margin(depth, cutNode && !ss->ttHit, improving, opponentWorsening)
-               - (ss - 1)->statScore / 272
-=======
         && eval - futility_margin(depth, cutNode && !ss->ttHit, improving)
-               - (ss - 1)->statScore / 260
->>>>>>> 4f3b9a52 (ergo simplification)
+               - (ss - 1)->statScore / 272
              >= beta
         && eval >= beta && (!ttData.move || ttCapture) && beta > VALUE_TB_LOSS_IN_MAX_PLY
         && eval < VALUE_TB_WIN_IN_MAX_PLY)
