858211fd7743245db4fca4966cf940c6b67ac262
Shawn Xu <xu107288696@gmail.com>
2024-09-28 00:23:39 -0700

    ttcapture nmp
    bench 1626017

--- src/search.cpp
+++ src/search.cpp
@@ -796,7 +796,7 @@ Value Search::Worker::search(
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 209, 6) + depth / 3 + 5;
+        Depth R = std::min(int(eval - beta) / 209, 6) + depth / 3 + 5 + ttCapture;
 
         ss->currentMove         = Move::null();
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
