df4343adf3e949be1e5ca7ec0b8488ccecb9a8d4
Shawn Xu <xu107288696@gmail.com>
2024-11-19 11:13:33 -0800

    multicut fail mid
    bench 644892

--- src/search.cpp
+++ src/search.cpp
@@ -1105,7 +1105,7 @@ moves_loop:  // When in check, search starts here
                 // singular (multiple moves fail high), and we can prune the whole
                 // subtree by returning a softbound.
                 else if (value >= beta && std::abs(value) < VALUE_TB_WIN_IN_MAX_PLY)
-                    return value + (value - beta) / 3;
+                    return value + std::min(ttData.value - value, 0) / 3;
 
                 // Negative extensions
                 // If other moves failed high over (ttValue - margin) without the
