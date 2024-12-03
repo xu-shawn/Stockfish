0e93dcddead0f979c017e142b54abc79472f5fc2
Shawn Xu <xu107288696@gmail.com>
2024-12-02 17:08:42 -0800

    pcb idea history updates
    bench 817106

--- src/search.cpp
+++ src/search.cpp
@@ -538,7 +538,7 @@ Value Search::Worker::search(
 
     // Dive into quiescence search when the depth reaches zero
     if (depth <= 0)
-        return qsearch<PvNode ? PV : NonPV>(pos, ss, alpha, beta);
+        return qsearch < PvNode ? PV : NonPV > (pos, ss, alpha, beta);
 
     // Limit the depth if extensions made it too large
     depth = std::min(depth, MAX_PLY - 1);
@@ -930,7 +930,12 @@ moves_loop:  // When in check, search starts here
     if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
         && std::abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         && std::abs(ttData.value) < VALUE_TB_WIN_IN_MAX_PLY)
+    {
+        if (ttData.move && !ttCapture)
+            update_quiet_histories(pos, ss, *thisThread, ttData.move, stat_bonus(depth));
+
         return probCutBeta;
+    }
 
     const PieceToHistory* contHist[] = {(ss - 1)->continuationHistory,
                                         (ss - 2)->continuationHistory,
