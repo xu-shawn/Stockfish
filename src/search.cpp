c6a38df95d735e969aee406b0b2082f1e6512c66
Shawn Xu <xu107288696@gmail.com>
2024-12-13 21:20:16 -0800

    clarity reductions
    bench 1573224

--- src/search.cpp
+++ src/search.cpp
@@ -538,7 +538,7 @@ Value Search::Worker::search(
 
     // Dive into quiescence search when the depth reaches zero
     if (depth <= 0)
-        return qsearch < PvNode ? PV : NonPV > (pos, ss, alpha, beta);
+        return qsearch<PvNode ? PV : NonPV>(pos, ss, alpha, beta);
 
     // Limit the depth if extensions made it too large
     depth = std::min(depth, MAX_PLY - 1);
@@ -1168,6 +1168,9 @@ moves_loop:  // When in check, search starts here
         // Increase reduction if ttMove is a capture but the current move is not a capture (~3 Elo)
         if (ttCapture && !capture)
             r += 1043 + (depth < 8) * 999;
+
+        if (std::abs(ss->staticEval - unadjustedStaticEval) > 80)
+            r -= 1024;
 
         // Increase reduction if next ply has a lot of fail high (~5 Elo)
         if ((ss + 1)->cutoffCnt > 3)
