438bcf9a32957fcf3dc1d8c338a187094c3a03e4
Shawn Xu <xu107288696@gmail.com>
2024-12-16 15:40:51 -0800

    try to balance out
    bench 1079115

--- src/search.cpp
+++ src/search.cpp
@@ -1161,6 +1161,8 @@ moves_loop:  // When in check, search starts here
 
         // These reduction adjustments have no proven non-linear scaling
 
+        r += 330;
+
         r -= std::min(std::abs(correctionValue) / 32768, 2048);
 
         // Increase reduction for cut nodes (~4 Elo)
