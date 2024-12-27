9125d4d8e2ccc1b39fc010a3e74ffed04f18be6f
Shawn Xu <xu107288696@gmail.com>
2024-12-27 02:45:58 -0800

    eval grain
    bench 1047892

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -84,6 +84,8 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     // Damp down the evaluation linearly when shuffling
     v -= v * pos.rule50_count() / 212;
 
+    v = v / 8 * 8;
+
     // Guarantee evaluation does not hit the tablebase range
     v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
 
