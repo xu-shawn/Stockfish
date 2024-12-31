b0a61e80eba07b8325a511daf3c66981fa0b2465
Shawn Xu <xu107288696@gmail.com>
2024-12-31 01:32:40 -0800

    correct history
    bench 1265114

--- src/search.cpp
+++ src/search.cpp
@@ -1374,7 +1374,16 @@ moves_loop:  // When in check, search starts here
     // If there is a move that produces search value greater than alpha,
     // we update the stats of searched moves.
     else if (bestMove)
+    {
         update_all_stats(pos, ss, *this, bestMove, prevSq, quietsSearched, capturesSearched, depth);
+        if (ttData.move && bestMove != ttData.move)
+        {
+            const auto ttHistEntry   = thisThread->mainHistory[~us][ttData.move.from_to()];
+            auto&      mainHistEntry = thisThread->mainHistory[~us][bestMove.from_to()];
+            if (ttHistEntry > mainHistEntry)
+                mainHistEntry << ttHistEntry - mainHistEntry;
+        }
+    }
 
     // Bonus for prior countermove that caused the fail low
     else if (!priorCapture && prevSq != SQ_NONE)
