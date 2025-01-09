95e9a6befd8df293838a7360265c6f1a3625aa01
Shawn Xu <xu107288696@gmail.com>
2025-01-09 10:19:22 -0800

    depth 1 lmr
    bench 1297421

--- src/search.cpp
+++ src/search.cpp
@@ -1192,15 +1192,16 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore * 1287 / 16384;
 
         // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
-        if (depth >= 2 && moveCount > 1)
+        if (moveCount > 1)
         {
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
             // beyond the first move depth.
             // To prevent problems when the max value is less than the min value,
             // std::clamp has been replaced by a more robust implementation.
-            Depth d = std::max(
-              1, std::min(newDepth - r / 1024, newDepth + !allNode + (PvNode && !bestMove)));
+            Depth d =
+              std::max<Depth>(depth >= 2, std::min(newDepth - r / 1024,
+                                                   newDepth + !allNode + (PvNode && !bestMove)));
 
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, d, true);
 
