98234ab6626c04fe0650a6f71e93fbbab47a477e
Shawn Xu <xu107288696@gmail.com>
2025-01-11 20:27:19 -0800

    take 2
    bench 1251434

--- src/search.cpp
+++ src/search.cpp
@@ -366,7 +366,7 @@ void Search::Worker::iterative_deepening() {
                 }
                 else if (bestValue >= beta)
                 {
-                    alpha += (bestValue - beta) / 4;
+                    alpha += std::min((bestValue - beta) / 4, 20);
                     beta = std::min(bestValue + delta, VALUE_INFINITE);
                     ++failedHighCnt;
                 }
