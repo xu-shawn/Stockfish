bc35e831bb8727bbebefe5bc128631af6810ddaa
Shawn Xu <xu107288696@gmail.com>
2025-01-11 18:38:56 -0800

    tweak 1
    bench 999324

--- src/thread.cpp
+++ src/thread.cpp
@@ -312,7 +312,7 @@ Thread* ThreadPool::get_best_thread() const {
 
     // Vote according to score and depth, and select the best thread
     auto thread_voting_value = [minScore](Thread* th) {
-        return (th->worker->rootMoves[0].score - minScore + 14) * int(th->worker->completedDepth);
+        return (th->worker->rootMoves[0].score - minScore + 12) * int(th->worker->completedDepth);
     };
 
     for (auto&& th : threads)
