fa9a48b21f18d5b4b0bc1d4ba3d6acb2f29c0d1e
Shawn Xu <xu107288696@gmail.com>
2025-01-11 20:24:30 -0800

    alpha tweak
    bench 1415768

--- src/search.cpp
+++ src/search.cpp
@@ -366,6 +366,7 @@ void Search::Worker::iterative_deepening() {
                 }
                 else if (bestValue >= beta)
                 {
+                    alpha += (bestValue - beta) / 4;
                     beta = std::min(bestValue + delta, VALUE_INFINITE);
                     ++failedHighCnt;
                 }
