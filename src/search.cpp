e03e8272283031b02756515804937d8bba4f0e6e
Shawn Xu <xu107288696@gmail.com>
2025-01-17 07:40:52 -0800

    lmr research
    bench 1439385

--- src/search.cpp
+++ src/search.cpp
@@ -1207,7 +1207,7 @@ moves_loop:  // When in check, search starts here
 
                 newDepth += doDeeperSearch - doShallowerSearch;
 
-                if (newDepth > d)
+                if (newDepth >= d)
                     value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode);
 
                 // Post LMR continuation history updates (~1 Elo)
