02319862b7957442c37c169cbed55c9440cc952b
Shawn Xu <xu107288696@gmail.com>
2025-01-17 22:00:16 -0800

    asdf
    bench 1339944

--- src/search.cpp
+++ src/search.cpp
@@ -786,7 +786,8 @@ Value Search::Worker::search(
                - (ss - 1)->statScore / 310
                + (ss->staticEval == eval) * (40 - std::abs(correctionValue) / 131072)
              >= beta
-        && eval >= beta && (!ttData.move || ttCapture) && !is_loss(beta) && !is_win(eval))
+        && eval >= beta && ((!ttData.move || !priorCapture) || ttCapture) && !is_loss(beta)
+        && !is_win(eval))
         return beta + (eval - beta) / 3;
 
     improving |= ss->staticEval >= beta + 97;
