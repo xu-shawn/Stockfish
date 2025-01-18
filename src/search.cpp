31d7ff1e919bcd98d34df8dd165538859cf33abc
Shawn Xu <xu107288696@gmail.com>
2025-01-18 15:48:46 -0800

    stuff
    bench 1627010

--- src/search.cpp
+++ src/search.cpp
@@ -1029,8 +1029,8 @@ moves_loop:  // When in check, search starts here
 
                 lmrDepth += history / 3459;
 
-                Value futilityValue =
-                  ss->staticEval + (bestValue < ss->staticEval - 47 ? 137 : 47) + 142 * lmrDepth;
+                Value futilityValue = ss->staticEval + (bestValue < ss->staticEval - 47 ? 137 : 47)
+                                    + 100 * priorCapture + 142 * lmrDepth;
 
                 // Futility pruning: parent node (~13 Elo)
                 if (!ss->inCheck && lmrDepth < 12 && futilityValue <= alpha)
