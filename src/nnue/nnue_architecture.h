a28633f6480d27bdc7a33e0ac240d387d116a9b5
Shawn Xu <xu107288696@gmail.com>
2025-01-20 14:58:45 -0800

    again
    bench 1800291

--- src/nnue/nnue_architecture.h
+++ src/nnue/nnue_architecture.h
@@ -126,7 +126,7 @@ struct NetworkArchitecture {
         // quantized form, but we want 1.0 to be equal to 600*OutputScale
         std::int32_t fwdOut =
           (buffer.fc_0_out[FC_0_OUTPUTS]) * (600 * OutputScale) / (127 * (1 << WeightScaleBits));
-        std::int32_t outputValue = (buffer.fc_2_out[0] * 1022 + fwdOut * 1026) / 2048;
+        std::int32_t outputValue = (buffer.fc_2_out[0] * 1025 + fwdOut * 1023) / 1024;
 
         return outputValue;
     }
