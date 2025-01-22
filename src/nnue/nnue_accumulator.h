97878c6a922c4b623a31817de79f786576bbc698
Shawn Xu <xu107288696@gmail.com>
2025-01-21 19:50:49 -0800

    3rd times the charm
    bench 1632964

--- src/nnue/nnue_accumulator.h
+++ src/nnue/nnue_accumulator.h
@@ -57,7 +57,7 @@ struct AccumulatorCaches {
     template<IndexType Size>
     struct alignas(CacheLineSize) Cache {
 
-        static constexpr std::size_t Duplication = 2;
+        static constexpr std::size_t Duplication = 3;
 
         struct alignas(CacheLineSize) Entry {
             BiasType       accumulation[Size];
