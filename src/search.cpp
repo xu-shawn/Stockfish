c495f74c73c62c347ef752c67ba7d79392f1871d
Shawn Xu <xu107288696@gmail.com>
2025-02-02 15:28:28 -0800

    stuff
    bench 3287424

--- src/search.cpp
+++ src/search.cpp
@@ -1612,7 +1612,7 @@ Value Search::Worker::qsearch(Position& pos, Stack* ss, Value alpha, Value beta)
             if (!givesCheck && move.to_sq() != prevSq && !is_loss(futilityBase)
                 && move.type_of() != PROMOTION)
             {
-                if (moveCount > 2)
+                if (!PvNode && moveCount > 2)
                     continue;
 
                 Value futilityValue = futilityBase + PieceValue[pos.piece_on(move.to_sq())];
