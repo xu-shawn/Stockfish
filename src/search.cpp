7e3897b7f7b5c2647c80a8d9e4f6d9c5f654aa7d
Shawn Xu <xu107288696@gmail.com>
2025-02-08 00:23:37 -0800

    check scaling
    bench 2727663

--- src/search.cpp
+++ src/search.cpp
@@ -1244,8 +1244,8 @@ moves_loop:  // When in check, search starts here
                 r += 1111;
 
             // Note that if expected reduction is high, we reduce search depth here
-            value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
-                                   newDepth - (r > 3554) - (r > 5373 && newDepth > 2), !cutNode);
+            value =
+              -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha, newDepth - (r > 3554), !cutNode);
         }
 
         // For PV nodes only, do a full PV search on the first move or after a fail high,
