ece17556b16c201eee54637e4b873ae2f7cfaaf1
Shawn Xu <xu107288696@gmail.com>
2025-02-08 15:18:30 -0800

    ttPv reduction
    bench 2748462

--- src/search.cpp
+++ src/search.cpp
@@ -1179,7 +1179,7 @@ moves_loop:  // When in check, search starts here
 
         // Increase reduction if next ply has a lot of fail high
         if ((ss + 1)->cutoffCnt > 3)
-            r += 981 + allNode * 833;
+            r += 981 + allNode * 833 - ss->ttPv * 600;
 
         // For first picked move (ttMove) reduce reduction
         else if (move == ttData.move)
