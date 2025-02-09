c497aa6cb185fead5a86165e5e6b32b4b9abaf95
Shawn Xu <xu107288696@gmail.com>
2025-02-08 18:33:34 -0800

    futility move count
    bench 2827883

--- src/search.cpp
+++ src/search.cpp
@@ -80,7 +80,7 @@ Value futility_margin(Depth d, bool noTtCutNode, bool improving, bool oppWorseni
 }
 
 constexpr int futility_move_count(bool improving, Depth depth) {
-    return (3 + depth * depth) / (2 - improving);
+    return (3072 + 1024 * depth * depth) / (3072 - 1024 * improving);
 }
 
 int correction_value(const Worker& w, const Position& pos, const Stack* const ss) {
