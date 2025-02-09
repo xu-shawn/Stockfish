5a4c8e57638c63fd5a48c421a6cd68b0030fd334
Shawn Xu <xu107288696@gmail.com>
2025-02-09 15:17:18 -0800

    take 2
    bench 2678043

--- src/search.cpp
+++ src/search.cpp
@@ -1397,11 +1397,13 @@ moves_loop:  // When in check, search starts here
     else if (!priorCapture && prevSq != SQ_NONE)
     {
         int bonusScale =
-          (118 * (depth > 5) + 36 * !allNode + 161 * ((ss - 1)->moveCount > 8)
-           + 133 * (!ss->inCheck && bestValue <= ss->staticEval - 107)
-           + 120 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 84)
-           + 81 * ((ss - 1)->isTTMove) + 100 * (ss->cutoffCnt <= 3) + 123 * (priorReduction <= 0)
-           + std::min(-(ss - 1)->statScore / 108, 320));
+          -90
+          + (118 * (depth > 5) + 36 * !allNode + 161 * ((ss - 1)->moveCount > 8)
+             + 133 * (!ss->inCheck && bestValue <= ss->staticEval - 107)
+             + 120 * (!(ss - 1)->inCheck && bestValue <= -(ss - 1)->staticEval - 84)
+             + 81 * ((ss - 1)->isTTMove) + 100 * (ss->cutoffCnt <= 3)
+
+             + 123 * (priorReduction <= 0) + std::min(-(ss - 1)->statScore / 108, 320));
 
         bonusScale = std::max(bonusScale, 0);
 
