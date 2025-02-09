a83db6e014e65ad42db00cca163ede23ab6cf138
Shawn Xu <xu107288696@gmail.com>
2025-02-08 22:42:18 -0800

    more conthist pruning
    bench 2806063

--- src/search.cpp
+++ src/search.cpp
@@ -1046,7 +1046,7 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -4107 * depth)
+                if (history < -3107 * depth + 1000)
                     continue;
 
                 history += 68 * thisThread->mainHistory[us][move.from_to()] / 32;
