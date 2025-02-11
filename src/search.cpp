c4c8c25303b2ad56694d7c49a5af591b9b1becd7
Shawn Xu <xu107288696@gmail.com>
2025-02-11 13:55:11 -0800

    cutnode cutoffcnt
    bench 2689363

--- src/search.cpp
+++ src/search.cpp
@@ -1345,7 +1345,7 @@ moves_loop:  // When in check, search starts here
                 if (value >= beta)
                 {
                     // (* Scaler) Especially if they make cutoffCnt increment more often.
-                    ss->cutoffCnt += (extension < 2) || PvNode;
+                    ss->cutoffCnt += (extension < 2) || PvNode || (cutNode && depth > 6);
                     assert(value >= beta);  // Fail high
                     break;
                 }
