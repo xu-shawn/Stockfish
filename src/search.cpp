465d16e97616920b04575c1434e2d3e1704fb11c
Shawn Xu <xu107288696@gmail.com>
2025-02-17 11:40:57 -0800

    tweak aspwin
    bench 2313268

--- src/search.cpp
+++ src/search.cpp
@@ -399,7 +399,7 @@ void Search::Worker::iterative_deepening() {
                 }
                 else if (bestValue >= beta)
                 {
-                    beta = std::min(bestValue + delta, VALUE_INFINITE);
+                    beta = std::min(beta + (bestValue - beta) * 3 / 4 + delta, VALUE_INFINITE);
                     ++failedHighCnt;
                 }
                 else
