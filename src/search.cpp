c64441efb059e42be002b43f94e51797dcaa5506
Shawn Xu <xu107288696@gmail.com>
2025-02-17 19:04:17 -0800

    more pruning
    bench 2132402

--- src/search.cpp
+++ src/search.cpp
@@ -1048,7 +1048,7 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -4107 * depth)
+                if (history < -(4107 - 1000 * PvNode) * depth)
                     continue;
 
                 history += 68 * thisThread->mainHistory[us][move.from_to()] / 32;
