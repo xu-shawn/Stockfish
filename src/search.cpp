5f2b0068f355c2561e29cf07f4dae683b5f9efa6
Shawn Xu <xu107288696@gmail.com>
2025-02-17 19:02:27 -0800

    futility move count
    bench 2677494

--- src/search.cpp
+++ src/search.cpp
@@ -1014,7 +1014,7 @@ moves_loop:  // When in check, search starts here
         if (!rootNode && pos.non_pawn_material(us) && !is_loss(bestValue))
         {
             // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-            if (moveCount >= futility_move_count(improving, depth))
+            if (moveCount >= futility_move_count(improving, depth) / (1 + PvNode))
                 mp.skip_quiet_moves();
 
             // Reduced depth of the next LMR search
