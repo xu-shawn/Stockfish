d9ecb7e29f3b674045cb4984a12989b71e0a9b26
Shawn Xu <xu107288696@gmail.com>
2025-02-23 15:39:06 -0800

    ttpv razoring
    bench 2458915

--- src/search.cpp
+++ src/search.cpp
@@ -814,7 +814,7 @@ Value Search::Worker::search(
     // Step 7. Razoring
     // If eval is really low, skip search entirely and return the qsearch value.
     // For PvNodes, we must have a guard against mates being returned.
-    if (!PvNode && eval < alpha - 446 - 303 * depth * depth)
+    if (!ss->ttPv && eval < alpha - 446 - 303 * depth * depth)
         return qsearch<NonPV>(pos, ss, alpha, beta);
 
     // Step 8. Futility pruning: child node
