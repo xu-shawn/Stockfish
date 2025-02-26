60bf15fb2b052038a973a23cb25aaa692ba434da
Shawn Xu <xu107288696@gmail.com>
2025-02-25 17:31:16 -0800

    remove beta lowering
    bench 2248669

--- src/search.cpp
+++ src/search.cpp
@@ -409,7 +409,6 @@ void Search::Worker::iterative_deepening() {
                 // otherwise exit the loop.
                 if (bestValue <= alpha)
                 {
-                    beta  = (alpha + beta) / 2;
                     alpha = std::max(bestValue - delta, -VALUE_INFINITE);
 
                     failedHighCnt = 0;
