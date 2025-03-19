91466d8c03851ede15bb04d7e68f069bc8a8da1d
Shawn Xu <xu107288696@gmail.com>
2025-03-19 16:59:04 -0700

    use ttDepth
    bench 2176482

--- src/search.cpp
+++ src/search.cpp
@@ -720,7 +720,7 @@ Value Search::Worker::search(
             // Bonus for a quiet ttMove that fails high
             if (!ttCapture)
                 update_quiet_histories(pos, ss, *this, ttData.move,
-                                       std::min(120 * depth - 75, 1241));
+                                       std::min(120 * ttData.depth - 75, 1241));
 
             // Extra penalty for early quiet moves of the previous ply
             if (prevSq != SQ_NONE && (ss - 1)->moveCount <= 3 && !priorCapture)
