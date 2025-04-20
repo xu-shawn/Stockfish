41f953e755d6c6276e1a266fe73baac8c9395756
Shawn Xu <xu107288696@gmail.com>
2025-04-20 03:40:59 -0700

    even crazier
    bench 1648671

--- src/search.cpp
+++ src/search.cpp
@@ -1161,7 +1161,7 @@ moves_loop:  // When in check, search starts here
                     extension = 1 + (value < singularBeta - doubleMargin)
                               + (value < singularBeta - tripleMargin);
 
-                    if (value < singularBeta - tripleMargin && !ss->ttPv && !ttCapture)
+                    if (value < singularBeta && !ss->ttPv)
                         extension = std::max(extension, ttData.depth + 1 - depth);
 
                     depth++;
