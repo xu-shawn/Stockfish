78382108fa015142ae1dd9f09b77ea686d5e5367
Shawn Xu <xu107288696@gmail.com>
2025-04-20 02:18:02 -0700

    insane ext
    bench 1913758

--- src/search.cpp
+++ src/search.cpp
@@ -1161,6 +1161,9 @@ moves_loop:  // When in check, search starts here
                     extension = 1 + (value < singularBeta - doubleMargin)
                               + (value < singularBeta - tripleMargin);
 
+                    if (value < singularBeta - tripleMargin && !ss->ttPv)
+                        extension = ttData.depth + 1 - depth;
+
                     depth++;
                 }
 
