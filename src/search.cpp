cde7565b7ad9586b720bf875ab1773dd4f60ad7a
Shawn Xu <xu107288696@gmail.com>
2025-04-20 19:13:07 -0700

    no pv node probcut
    bench 1710376

--- src/search.cpp
+++ src/search.cpp
@@ -916,7 +916,7 @@ Value Search::Worker::search(
     // If we have a good enough capture (or queen promotion) and a reduced search
     // returns a value much above beta, we can (almost) safely prune the previous move.
     probCutBeta = beta + 185 - 58 * improving;
-    if (depth >= 3
+    if (!PvNode && depth >= 3
         && !is_decisive(beta)
         // If value from transposition table is lower than probCutBeta, don't attempt
         // probCut there and in further interactions with transposition table cutoff
