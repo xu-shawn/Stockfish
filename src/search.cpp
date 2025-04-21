3c6a09ff1f634edf125ad88d49423587c0cd85a6
Shawn Xu <xu107288696@gmail.com>
2025-04-21 08:31:44 -0700

    all node extension
    bench 2204493

--- src/search.cpp
+++ src/search.cpp
@@ -1223,6 +1223,8 @@ moves_loop:  // When in check, search starts here
         // Increase reduction for cut nodes
         if (cutNode)
             r += 2784 + 1038 * !ttData.move;
+        else
+            r -= 512;
 
         // Increase reduction if ttMove is a capture but the current move is not a capture
         if (ttCapture && !capture)
