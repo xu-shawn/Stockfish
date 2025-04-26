758e1ca722e4b4ef6f565266951b7ade42a278f3
Shawn Xu <xu107288696@gmail.com>
2025-04-26 13:57:33 -0700

    no qsearch from cutnodes
    bench 2124670

--- src/search.cpp
+++ src/search.cpp
@@ -1273,7 +1273,7 @@ moves_loop:  // When in check, search starts here
         r -= ss->statScore * 1582 / 16384;
 
         // Step 17. Late moves reduction / extension (LMR)
-        if (depth >= 2 && moveCount > 1)
+        if (depth >= 1 + allNode && moveCount > 1)
         {
             // In general we want to cap the LMR depth search at newDepth, but when
             // reduction is negative, we allow this move a limited search extension
