e7b4968aeff215cce10f572391c786fc7eb8318b
Shawn Xu <xu107288696@gmail.com>
2025-05-24 21:47:13 -0700

    cutoffcnt
    bench 3165393

--- src/search.cpp
+++ src/search.cpp
@@ -1221,10 +1221,6 @@ moves_loop:  // When in check, search starts here
         if (ttCapture)
             r += 1210 + (depth < 8) * 963;
 
-        // Increase reduction if next ply has a lot of fail high
-        if ((ss + 1)->cutoffCnt > 2)
-            r += 1036 + allNode * 848;
-
         if (!capture && !givesCheck && ss->quietMoveStreak >= 2)
             r += (ss->quietMoveStreak - 1) * 50;
 
