68ff88427f60a48f469fbaa848df51f0785d5871
Shawn Xu <xu107288696@gmail.com>
2025-05-25 11:08:19 -0700

    remove quiet move streak
    bench 2184701

--- src/search.h
+++ src/search.h
@@ -76,7 +76,6 @@ struct Stack {
     int                         cutoffCnt;
     int                         reduction;
     bool                        isPvNode;
-    int                         quietMoveStreak;
 };
 
 
