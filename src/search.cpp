0cd0cacc142fe5def1b59a11f360a030279f5891
Shawn Xu <xu107288696@gmail.com>
2025-05-25 13:43:15 -0700

    asdf
    bench 2124191

--- src/search.cpp
+++ src/search.cpp
@@ -1220,8 +1220,8 @@ moves_loop:  // When in check, search starts here
         if ((ss + 1)->cutoffCnt > 2)
             r += 1036 + allNode * 848;
 
-        if (!capture && !givesCheck && ss->quietMoveStreak >= 2)
-            r += (ss->quietMoveStreak - 1) * 50;
+        if (!capture && !givesCheck)
+            r += 100;
 
         // For first picked move (ttMove) reduce reduction
         if (move == ttData.move)
