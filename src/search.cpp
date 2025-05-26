4fbfa25c8ec1e83029baf8e6a57ffe75dff0ef60
Shawn Xu <xu107288696@gmail.com>
2025-05-26 12:17:04 -0700

    do research
    Bench: 2303613

--- src/search.cpp
+++ src/search.cpp
@@ -1259,7 +1259,7 @@ moves_loop:  // When in check, search starts here
             // Do a full-depth search when reduced LMR search fails high
             // (*Scaler) Usually doing more shallower searches
             // doesn't scale well to longer TCs
-            if (value > alpha && d < newDepth)
+            if (value > alpha)
             {
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
