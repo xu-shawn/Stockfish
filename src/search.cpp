d307c73d754fbf814ab81e32df39c4484ec02bba
Shawn Xu <xu107288696@gmail.com>
2025-05-31 15:55:38 -0700

    stuff
    Bench: 2394289

--- src/search.cpp
+++ src/search.cpp
@@ -1289,7 +1289,7 @@ moves_loop:  // When in check, search starts here
 
             // Note that if expected reduction is high, we reduce search depth here
             value = -search<NonPV>(pos, ss + 1, -(alpha + 1), -alpha,
-                                   newDepth - (r > 3564) - (r > 4969 && newDepth > 2), !cutNode);
+                                   newDepth - (move != ttData.move && r > 3564) - (move != ttData.move && r > 4969 && newDepth > 2), !cutNode);
         }
 
         // For PV nodes only, do a full PV search on the first move or after a fail high,
