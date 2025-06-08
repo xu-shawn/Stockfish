8242c447389e6827a90dc6d6c6b6d4a007d33348
Shawn Xu <xu107288696@gmail.com>
2025-06-07 17:14:21 -0700

    eval symmetric optimism
    Bench: 2239561

--- src/search.cpp
+++ src/search.cpp
@@ -327,7 +327,7 @@ void Search::Worker::iterative_deepening() {
             beta      = std::min(avg + delta, VALUE_INFINITE);
 
             // Adjust optimism based on root move's averageScore
-            optimism[us]  = 137 * avg / (std::abs(avg) + 91);
+            optimism[us]  = 137 * avg / (avg + 91);
             optimism[~us] = -optimism[us];
 
             // Start with a small aspiration window and, in the case of a fail
