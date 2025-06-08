56170ba048ee0b39a2e4934f4700b781e56b1fcc
Shawn Xu <xu107288696@gmail.com>
2025-06-07 17:32:30 -0700

    stuff
    Bench: 2097236

--- src/search.cpp
+++ src/search.cpp
@@ -327,7 +327,7 @@ void Search::Worker::iterative_deepening() {
             beta      = std::min(avg + delta, VALUE_INFINITE);
 
             // Adjust optimism based on root move's averageScore
-            optimism[us]  = 178 * avg / (avg + 118);
+            optimism[us]  = 178 * avg / (std::abs(avg) + 118);
             optimism[~us] = -optimism[us];
 
             // Start with a small aspiration window and, in the case of a fail
