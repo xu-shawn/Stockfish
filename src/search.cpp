de138c437abdcd116c6bbc4bfefb0360d04a0717
Shawn Xu <xu107288696@gmail.com>
2025-06-19 23:53:44 -0700

    asdf
    Bench: 2055544

--- src/search.cpp
+++ src/search.cpp
@@ -1212,7 +1212,7 @@ moves_loop:  // When in check, search starts here
 
         // For first picked move (ttMove) reduce reduction
         if (move == ttData.move)
-            r -= 2006 + (ss->ply * 2 > rootDepth * 3);
+            r -= 2006 + (ss->ply * 2 > rootDepth * 3) * 1024;
 
         if (capture)
             ss->statScore =
