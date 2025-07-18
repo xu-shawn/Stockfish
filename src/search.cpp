7f6e7de0b8d4eb08c5beb739abe36f2bd3782d27
Shawn Xu <xu107288696@gmail.com>
2025-07-18 14:34:50 -0700

    linearize mcp
    Bench: 2180839

--- src/search.cpp
+++ src/search.cpp
@@ -1033,7 +1033,7 @@ moves_loop:  // When in check, search starts here
         if (!rootNode && pos.non_pawn_material(us) && !is_loss(bestValue))
         {
             // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
-            if (moveCount >= (3 + depth * depth) / (2 - improving))
+            if (depth <= 5 && moveCount >= (3 + 4 * depth) / (2 - improving))
                 mp.skip_quiet_moves();
 
             // Reduced depth of the next LMR search
