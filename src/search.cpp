c0025cdd0b8bb5ea931cfe5b81e0c98b289ddc65
Shawn Xu <xu107288696@gmail.com>
2025-07-23 17:51:03 -0700

    simpl hist pruning
    Bench: 2543274

--- src/search.cpp
+++ src/search.cpp
@@ -1078,7 +1078,7 @@ moves_loop:  // When in check, search starts here
                   + thisThread->pawnHistory[pawn_structure_index(pos)][movedPiece][move.to_sq()];
 
                 // Continuation history based pruning
-                if (history < -4229 * depth)
+                if (depth <= 3 && history < -7500)
                     continue;
 
                 history += 68 * thisThread->mainHistory[us][move.from_to()] / 32;
