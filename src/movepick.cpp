436c92136d3b558f6daff0a90e9ce26bcb47c0b9
Shawn Xu <xu107288696@gmail.com>
2025-08-02 23:12:04 -0700

    rm check bonus
    Bench: 2862168

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -165,9 +165,6 @@ ExtMove* MovePicker::score(MoveList<Type>& ml) {
             m.value += (*continuationHistory[3])[pc][to];
             m.value += (*continuationHistory[5])[pc][to];
 
-            // bonus for checks
-            m.value += (bool(pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 16384;
-
             // penalty for moving to a square threatened by a lesser piece
             // or bonus for escaping an attack by a lesser piece.
             if (KNIGHT <= pt && pt <= QUEEN)
