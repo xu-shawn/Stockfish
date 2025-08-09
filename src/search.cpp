4524cbbe71deded9474e5620e1076a88c7f44b3a
Shawn Xu <xu107288696@gmail.com>
2025-08-09 00:17:58 -0700

    more probcut depth decrease
    Bench: 3234204

--- src/search.cpp
+++ src/search.cpp
@@ -914,8 +914,7 @@ Value Search::Worker::search(
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);
 
         MovePicker mp(pos, ttData.move, probCutBeta - ss->staticEval, &captureHistory);
-        Depth      dynamicReduction = (ss->staticEval - beta) / 300;
-        Depth      probCutDepth     = std::max(depth - 5 - dynamicReduction, 0);
+        Depth      probCutDepth     = std::max(depth - 6, 0);
 
         while ((move = mp.next_move()) != Move::none())
         {
