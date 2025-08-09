8a114d7f14555c24cf60c7fcb25acab69f8ca19c
Shawn Xu <xu107288696@gmail.com>
2025-08-09 00:16:58 -0700

    rm prelim pcb
    Bench: 3515698

--- src/search.cpp
+++ src/search.cpp
@@ -930,11 +930,6 @@ Value Search::Worker::search(
 
             do_move(pos, move, st, ss);
 
-            // Perform a preliminary qsearch to verify that the move holds
-            value = -qsearch<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1);
-
-            // If the qsearch held, perform the regular search
-            if (value >= probCutBeta && probCutDepth > 0)
                 value = -search<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1, probCutDepth,
                                        !cutNode);
 
