38485f362c02f992c91efd5e8d0739a2073f8ebf
Shawn Xu <xu107288696@gmail.com>
2025-08-24 15:18:56 -0700

    asdf
    Bench: 2693376

--- src/search.cpp
+++ src/search.cpp
@@ -1133,7 +1133,7 @@ moves_loop:  // When in check, search starts here
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
 
-                depth++;
+                depth += 1 + (threadIdx & 1);
             }
 
             // Multi-cut pruning
