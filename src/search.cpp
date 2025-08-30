7185c14eea6b4b2ff139cacdea9db7cda44864c9
Shawn Xu <xu107288696@gmail.com>
2025-08-30 02:14:50 -0700

    probcut depth clamp (search explosion fix)
    Bench: 2488276

--- src/search.cpp
+++ src/search.cpp
@@ -917,8 +917,7 @@ Value Search::Worker::search(
         assert(probCutBeta < VALUE_INFINITE && probCutBeta > beta);
 
         MovePicker mp(pos, ttData.move, probCutBeta - ss->staticEval, &captureHistory);
-        Depth      dynamicReduction = (ss->staticEval - beta) / 306;
-        Depth      probCutDepth     = std::max(depth - 5 - dynamicReduction, 0);
+        Depth probCutDepth = std::clamp(depth - 5 - (ss->staticEval - beta) / 306, 0, depth - 1);
 
         while ((move = mp.next_move()) != Move::none())
         {
