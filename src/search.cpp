5ec75f51555541f33cb5d768a179127fb5750f90
Shawn Xu <xu107288696@gmail.com>
2025-08-31 18:38:42 -0700

    stuff
    Bench: 2808975

--- src/search.cpp
+++ src/search.cpp
@@ -695,9 +695,7 @@ Value Search::Worker::search(
         && is_valid(ttData.value)  // Can happen when !ttHit or when access race in probe()
         && (ttData.bound & (ttData.value >= beta ? BOUND_LOWER : BOUND_UPPER))
         && (cutNode == (ttData.value >= beta) || depth > 5)
-        // avoid a TT cutoff if the rule50 count is high and the TT move is zeroing
-        && (depth > 8 || ttData.move == Move::none() || pos.rule50_count() < 80
-            || (!ttCapture && type_of(pos.moved_piece(ttData.move)) != PAWN)))
+        && (depth > 8 || pos.rule50_count() < 80))
     {
         // If ttMove is quiet, update move sorting heuristics on TT hit
         if (ttData.move && ttData.value >= beta)
