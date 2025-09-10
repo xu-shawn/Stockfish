342bc86a30c87b34605c68fe94f01587331176b8
Shawn Xu <xu107288696@gmail.com>
2025-09-10 13:12:14 -0700

    asdf
    Bench: 3238361

--- src/search.cpp
+++ src/search.cpp
@@ -1039,7 +1039,7 @@ moves_loop:  // When in check, search starts here
 
         // Step 14. Pruning at shallow depth.
         // Depth conditions are important for mate finding.
-        if (!rootNode && pos.non_pawn_material(us) && !is_loss(bestValue))
+        if (!rootNode && pos.non_pawn_material(us) > PieceValue[movedPiece] && !is_loss(bestValue))
         {
             // Skip quiet moves if movecount exceeds our FutilityMoveCount threshold
             if (moveCount >= (3 + depth * depth) / (2 - improving))
