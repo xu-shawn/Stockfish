d8c69c79a8210d785088fb1dee09dd91eb156769
Shawn Xu <xu107288696@gmail.com>
2025-09-10 13:12:46 -0700

    asd
    Bench: 3201589

--- src/search.cpp
+++ src/search.cpp
@@ -1066,8 +1066,7 @@ moves_loop:  // When in check, search starts here
                 // SEE based pruning for captures and checks
                 // Avoid pruning sacrifices of our last piece for stalemate
                 int margin = std::max(157 * depth + captHist / 29, 0);
-                if ((alpha >= VALUE_DRAW || pos.non_pawn_material(us) != PieceValue[movedPiece])
-                    && !pos.see_ge(move, -margin))
+                if (alpha >= VALUE_DRAW && !pos.see_ge(move, -margin))
                     continue;
             }
             else
