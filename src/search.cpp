40e46c903c657c45a375b0ac420262c721375656
Shawn Xu <xu107288696@gmail.com>
2025-09-10 11:56:29 -0700

    simp
    Bench: 2971853

--- src/search.cpp
+++ src/search.cpp
@@ -1357,7 +1357,7 @@ moves_loop:  // When in check, search starts here
                 if (value >= beta)
                 {
                     // (*Scaler) Especially if they make cutoffCnt increment more often.
-                    ss->cutoffCnt += (extension < 2) || PvNode;
+                    ss->cutoffCnt++;
                     assert(value >= beta);  // Fail high
                     break;
                 }
