6a8c7cdb681d78ff0c53e35fb77b0d0874443255
Shawn Xu <xu107288696@gmail.com>
2025-09-10 17:55:01 -0700

    twk
    Bench: 2915445

--- src/search.cpp
+++ src/search.cpp
@@ -1131,9 +1131,9 @@ moves_loop:  // When in check, search starts here
             if (value < singularBeta)
             {
                 int corrValAdj   = std::abs(correctionValue) / 229958;
-                int doubleMargin = -4 + 198 * PvNode - 212 * !ttCapture - corrValAdj
+                int doubleMargin = -14 + 198 * PvNode - 212 * !ttCapture - corrValAdj
                                  - 921 * ttMoveHistory / 127649 - (ss->ply > rootDepth) * 45;
-                int tripleMargin = 76 + 308 * PvNode - 250 * !ttCapture + 92 * ss->ttPv - corrValAdj
+                int tripleMargin = 56 + 308 * PvNode - 250 * !ttCapture + 92 * ss->ttPv - corrValAdj
                                  - (ss->ply * 2 > rootDepth * 3) * 52;
 
                 extension =
