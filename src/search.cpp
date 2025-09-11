8d49a54f4062a1aef41e1b10871d4ad07dc17117
Shawn Xu <xu107288696@gmail.com>
2025-09-10 19:25:13 -0700

    no way
    Bench: 2540256

--- src/search.cpp
+++ src/search.cpp
@@ -972,7 +972,7 @@ moves_loop:  // When in check, search starts here
 
     // Step 12. A small Probcut idea
     probCutBeta = beta + 418;
-    if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 4 && ttData.value >= probCutBeta
+    if ((ttData.bound & BOUND_LOWER) && ttData.depth >= depth - 6 && ttData.value >= probCutBeta
         && !is_decisive(beta) && is_valid(ttData.value) && !is_decisive(ttData.value))
         return probCutBeta;
 
