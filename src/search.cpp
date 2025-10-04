5d47f8e8575c0b6740e1490642c4d40abfc6706b
Shawn Xu <xu107288696@gmail.com>
2025-10-04 15:44:12 -0700

    take 6
    Bench: 1985382

--- src/search.cpp
+++ src/search.cpp
@@ -781,7 +781,7 @@ Value Search::Worker::search(
     // Step 6. Static evaluation of the position
     Value      unadjustedStaticEval = VALUE_NONE;
     const auto correctionValue      = correction_value(*this, pos, ss);
-    correctionMetaHistory[us] << correctionValue / 16384;
+    correctionMetaHistory[us] << correctionValue / 32768;
     if (ss->inCheck)
     {
         // Skip early pruning when in check
