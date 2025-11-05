b6b59d5c70da3ea186464b036c0017de84f5ecb5
Timothy Herchen <timothy.herchen@gmail.com>
2025-11-04 23:04:04 -0800

    hide behind ifdef

--- src/nnue/nnue_accumulator.cpp
+++ src/nnue/nnue_accumulator.cpp
@@ -358,8 +358,15 @@ struct AccumulatorUpdateContext {
                 auto*           column =
                   reinterpret_cast<const vec_i8_t*>(&featureTransformer.threatWeights[offset]);
 
+#ifdef USE_NEON
+                for (IndexType k = 0; k < Tiling::NumRegs; k += 2) {
+                    acc[k] = vec_sub_16(acc[k], vmovl_s8(vget_low_s8(column[k / 2])));
+                    acc[k + 1] = vec_sub_16(acc[k + 1], vmovl_high_s8(column[k / 2]));
+                }
+#else
                 for (IndexType k = 0; k < Tiling::NumRegs; ++k)
                     acc[k] = vec_sub_16(acc[k], vec_convert_8_16(column[k]));
+#endif
             }
 
             for (IndexType i = 0; i < added.size(); ++i)
@@ -369,8 +376,15 @@ struct AccumulatorUpdateContext {
                 auto*           column =
                   reinterpret_cast<const vec_i8_t*>(&featureTransformer.threatWeights[offset]);
 
+#ifdef USE_NEON
+                for (IndexType k = 0; k < Tiling::NumRegs; k += 2) {
+                    acc[k] = vec_add_16(acc[k], vmovl_s8(vget_low_s8(column[k / 2])));
+                    acc[k + 1] = vec_add_16(acc[k + 1], vmovl_high_s8(column[k / 2]));
+                }
+#else
                 for (IndexType k = 0; k < Tiling::NumRegs; ++k)
                     acc[k] = vec_add_16(acc[k], vec_convert_8_16(column[k]));
+#endif
             }
 
             for (IndexType k = 0; k < Tiling::NumRegs; k++)
@@ -806,8 +820,15 @@ void update_threats_accumulator_full(const FeatureTransformer<Dimensions>& featu
             auto*           column =
               reinterpret_cast<const vec_i8_t*>(&featureTransformer.threatWeights[offset]);
 
+#ifdef USE_NEON
+                for (IndexType k = 0; k < Tiling::NumRegs; k += 2) {
+                    acc[k] = vec_add_16(acc[k], vmovl_s8(vget_low_s8(column[k / 2])));
+                    acc[k + 1] = vec_add_16(acc[k + 1], vmovl_high_s8(column[k / 2]));
+                }
+#else
             for (IndexType k = 0; k < Tiling::NumRegs; ++k)
                 acc[k] = vec_add_16(acc[k], vec_convert_8_16(column[k]));
+#endif
         }
 
         for (IndexType k = 0; k < Tiling::NumRegs; k++)
