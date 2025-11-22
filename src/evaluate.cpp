83ceb87afd6691c5f1e83530b3c174461a3fae48
Shawn Xu <xu107288696@gmail.com>
2025-11-21 23:32:09 -0800

    eval stuff
    Bench: 2709951

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -65,7 +65,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     Value nnue = (125 * psqt + 131 * positional) / 128;
 
     // Re-evaluate the position when higher eval accuracy is worth the time spent
-    if (smallNet && (std::abs(nnue) < 277))
+    if (smallNet && (std::abs(nnue) < 350))
     {
         std::tie(psqt, positional) = networks.big.evaluate(pos, accumulators, caches.big);
         nnue                       = (125 * psqt + 131 * positional) / 128;
