c99e878f10b4c3e986df83c60597b1fc618bd2f0
Shawn Xu <xu107288696@gmail.com>
2025-12-02 09:41:24 -0800

    remove
    Bench: 2389807

--- src/search.cpp
+++ src/search.cpp
@@ -1125,7 +1125,7 @@ moves_loop:  // When in check, search starts here
         // and lower extension margins scale well.
         if (!rootNode && move == ttData.move && !excludedMove && depth >= 6 + ss->ttPv
             && is_valid(ttData.value) && !is_decisive(ttData.value) && (ttData.bound & BOUND_LOWER)
-            && ttData.depth >= depth - 3 && !isShuffling(move, ss, pos))
+            && ttData.depth >= depth - 3)
         {
             Value singularBeta  = ttData.value - (53 + 75 * (ss->ttPv && !PvNode)) * depth / 60;
             Depth singularDepth = newDepth / 2;
