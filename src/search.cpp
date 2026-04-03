71017d40d71d767be3ceebd87857475cdd575db8
Shawn Xu <xu107288696@gmail.com>
2026-04-03 00:12:40 -0700

    suspicious tt value extensions
    Bench: 3019167

--- src/search.cpp
+++ src/search.cpp
@@ -1155,10 +1155,13 @@ moves_loop:  // When in check, search starts here
             if (value < singularBeta)
             {
                 int corrValAdj   = std::abs(correctionValue) / 210590;
-                int doubleMargin = -4 + 212 * PvNode - 182 * !ttCapture - corrValAdj
-                                 - 906 * ttMoveHistory / 116517 - (ss->ply > rootDepth) * 44;
-                int tripleMargin = 73 + 320 * PvNode - 218 * !ttCapture + 92 * ss->ttPv - corrValAdj
-                                 - (ss->ply > rootDepth) * 45;
+                int doubleMargin = -4 + 212 * PvNode
+                                 - (182 + 50 * (ttData.value >= ss->staticEval + 100)) * !ttCapture
+                                 - corrValAdj - 906 * ttMoveHistory / 116517
+                                 - (ss->ply > rootDepth) * 44;
+                int tripleMargin = 73 + 320 * PvNode
+                                 - (218 + 50 * (ttData.value >= ss->staticEval + 100)) * !ttCapture
+                                 + 92 * ss->ttPv - corrValAdj - (ss->ply > rootDepth) * 45;
 
                 extension =
                   1 + (value < singularBeta - doubleMargin) + (value < singularBeta - tripleMargin);
