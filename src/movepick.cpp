991d06a5ac2782ca5984fec3414b6afff432ac70
Shawn Xu <xu107288696@gmail.com>
2026-04-04 00:18:09 -0700

    stuff
    Bench: 3112989

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -199,14 +199,13 @@ ExtMove* MovePicker::score(const MoveList<Type>& ml) {
             m.value += (*continuationHistory[5])[pc][to];
 
             // bonus for checks
-            m.value += (bool(pos.check_squares(pt) & to) && pos.see_ge(m, -75)) * 16384;
+            m.value += (bool(pos.check_squares(pt) & to) && pos.see_ge(m, -75))
+                     * (1 + bool(safeThreats[pt] & to)) * 16384;
 
             // penalty for moving to a square threatened by a lesser piece
             // or bonus for escaping an attack by a lesser piece.
             int v = 20 * (bool(threatByLesser[pt] & from) - bool(threatByLesser[pt] & to));
             m.value += PieceValue[pt] * v;
-
-            m.value += 9000 * bool(safeThreats[pt] & to);
 
             if (ply < LOW_PLY_HISTORY_SIZE)
                 m.value += 8 * (*lowPlyHistory)[ply][m.raw()] / (1 + ply);
