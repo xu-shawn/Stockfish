50a01c8c5f20b317ab5261b8f346b71722a9ff03
Shawn Xu <xu107288696@gmail.com>
2026-06-30 13:11:11 -0700

    4th stage ablation 1
    Bench: 2849654

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultName "nn-2df6cbc73bcd.nnue"
+#define EvalFileDefaultName "nn-17504e8c3881.nnue"
 
 namespace NNUE {
 class Network;
