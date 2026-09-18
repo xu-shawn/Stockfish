9c11e2317a02a87be0e2c0c4347c2a0370561e1c
Michael Chaly <Vizvezdenec@gmail.com>
2026-09-18 20:59:44 +0200

    Allow razoring to be performed only at allNodes
    Idea is pretty similar to what we do in null move pruning. Null move pruning is performed only in cutnodes and is a fail high cutoff.
    Razoring is a fail low cutoff so it makes sense to perform it only at nodes of the opposite type to cutNode - so allNodes.
    Margin adjusted to produce roughly the same number of cutoffs.
    Passed STC:
    https://tests.stockfishchess.org/tests/view/6aa9a826f648bce025258401
    LLR: 2.95 (-2.94,2.94) <0.00,2.00>
    Total: 102496 W: 26351 L: 25952 D: 50193
    Ptnml(0-2): 101, 11855, 26980, 12168, 144
    Passed LTC:
    https://tests.stockfishchess.org/tests/view/6aabc542dbd128aac8fd51e1
    LLR: 2.94 (-2.94,2.94) <0.50,2.50>
    Total: 42672 W: 11154 L: 10833 D: 20685
    Ptnml(0-2): 5, 4363, 12283, 4676, 9
    
    closes https://github.com/official-stockfish/Stockfish/pull/7173
    
    Bench: 1452016

--- src/search.cpp
+++ src/search.cpp
@@ -999,7 +999,7 @@ Value Search::Worker::search(
 
     // Step 8. Razoring
     // If eval is really low, skip search entirely and return the qsearch value
-    if (!PvNode && eval < alpha - 482 * depth && !seekMate)
+    if (allNode && eval < alpha - 342 * depth && !seekMate)
         return qsearch<NonPV>(pos, ss, alpha, beta);
 
     // Step 9. Futility pruning: child node
