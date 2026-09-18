7a372f3fd71a036ed35d2d474f44a3138be69a0f
Stefan Geschwentner <stgeschwentner@gmail.com>
2026-09-18 20:59:16 +0200

    Tweak evaluation correction factor.
    Update (ss - 6)->continuationCorrectionHistory and apply it to correction value calculation. Also scale down the other weights for compensation.
    
    Passed STC:
    LLR: 2.95 (-2.94,2.94) <0.00,2.00>
    Total: 96576 W: 24676 L: 24283 D: 47617
    Ptnml(0-2): 133, 11165, 25327, 11502, 161
    https://tests.stockfishchess.org/tests/view/6a9adaeea8284b37a0a047e5
    
    Passed LTC:
    LLR: 2.94 (-2.94,2.94) <0.50,2.50>
    Total: 222570 W: 58024 L: 57343 D: 107203
    Ptnml(0-2): 60, 23229, 64029, 23904, 63
    https://tests.stockfishchess.org/tests/view/6a9d3e22a8284b37a0a04b67
    
    Passed non-regression VVLTC:
    LLR: 2.94 (-2.94,2.94) <-1.75,0.25>
    Total: 37320 W: 9914 L: 9735 D: 17671
    Ptnml(0-2): 3, 3133, 12210, 3310, 4
    https://tests.stockfishchess.org/tests/view/6aa5a36ba8284b37a0a05aa3
    
    closes https://github.com/official-stockfish/Stockfish/pull/7163
    
    Bench: 1399065

--- src/search.cpp
+++ src/search.cpp
@@ -92,12 +92,13 @@ int correction_value(const Worker& w, const Position& pos, const Stack* const ss
     const int   bnpcv  = shared.nonpawn_correction_entry<BLACK>(pos)[us].nonPawnBlack;
     const int   cntcv =
       m.is_ok()
-          ? 8761
-            * ((*(ss - 2)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
-               + (*(ss - 4)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()])
-          : 64049;
+          ? 7885
+              * ((*(ss - 2)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
+                 + (*(ss - 4)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()])
+            + 6307 * (*(ss - 6)->continuationCorrectionHistory)[pos.piece_on(m.to_sq())][m.to_sq()]
+          : 80695;
 
-    return 15341 * pcv + 10569 * micv + 12906 * (wnpcv + bnpcv) + cntcv;
+    return 13806 * pcv + 9512 * micv + 11615 * (wnpcv + bnpcv) + cntcv;
 }
 
 // Add correctionHistory value to raw staticEval and guarantee evaluation
@@ -127,6 +128,7 @@ void update_correction_history(const Position& pos,
         const Piece  pc = pos.piece_on(to);
         (*(ss - 2)->continuationCorrectionHistory)[pc][to] << bonus * 130 / 128;
         (*(ss - 4)->continuationCorrectionHistory)[pc][to] << bonus * 70 / 128;
+        (*(ss - 6)->continuationCorrectionHistory)[pc][to] << bonus * 35 / 128;
     }
 }
 
@@ -660,6 +662,7 @@ void Search::Worker::do_move(
 
         prefetch(&(*(ss - 1)->continuationCorrectionHistory)[pc][to]);
         prefetch(&(*(ss - 3)->continuationCorrectionHistory)[pc][to]);
+        prefetch(&(*(ss - 5)->continuationCorrectionHistory)[pc][to]);
     }
 
     ++nodes;
