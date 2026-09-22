008f621e8fa031d87d4c19e150ba6f38fea8e09e
Shawn Xu <xu107288696@gmail.com>
2026-09-22 18:44:23 -0500

    tweak piece count skipping distribution
    Bench: 1343766

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultName "nn-134a887f4c8f.nnue"
+#define EvalFileDefaultName "nn-3421ea39acbf.nnue"
 
 namespace NNUE {
 class Network;
